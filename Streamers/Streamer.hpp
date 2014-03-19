64037ccc686d16733f10dfcd8c03e4d04019ea99
Erik Smistad <smistad@idi.ntnu.no>
2014-03-20 00:50:35 +0100

    added streamer base class

---
+++ Streamers/Streamer.hpp
@@ -0,0 +1,16 @@
+#ifndef STREAMER_HPP_
+#define STREAMER_HPP_
+
+#include "PipelineObject.hpp"
+namespace fast {
+
+class Streamer : public PipelineObject {
+
+};
+
+}; // end namespace fast
+
+
+
+
+#endif /* STREAMER_HPP_ */
