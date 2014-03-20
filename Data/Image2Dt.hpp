7fb82a787927b3ad90e546575e0637fb887b752e
Erik Smistad <smistad@idi.ntnu.no>
2014-03-20 01:30:19 +0100

    added some comments to Image2Dt

--- Data/Image2Dt.hpp
+++ Data/Image2Dt.hpp
@@ -4,8 +4,8 @@
 #include "PipelineObject.hpp"
 #include "Image2D.hpp"
 #include <vector>
-namespace fast {
 
+namespace fast {
 
 class Image2Dt : public PipelineObject {
     public:
@@ -14,12 +14,20 @@ class Image2Dt : public PipelineObject {
     private:
         // Flag whether to keep and store all frames or only use the current
         bool mKeepAllFrames;
+
+        // If the flag mKeepAllFrames is set to false, this vector will have
+        // a max size of 1
         std::vector<Image2DPtr> mFrames;
+
+        // Keep track of which frame is next, only used when mKeepAllFrames is
+        // set to true
+        unsigned long mCurrentFrame;
+
         void execute();
 };
 
 typedef boost::shared_ptr<Image2Dt> Image2DtPtr;
 
-}; // end namespace fast
+} // end namespace fast
 
 #endif
