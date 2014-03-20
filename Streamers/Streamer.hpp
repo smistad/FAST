85cc96e06ae875f4676f26f5820a1de64e444576
Erik Smistad <smistad@idi.ntnu.no>
2014-03-20 01:46:36 +0100

    added a mutex to the streamer object

--- Streamers/Streamer.hpp
+++ Streamers/Streamer.hpp
@@ -2,17 +2,25 @@
 #define STREAMER_HPP_
 
 #include "PipelineObject.hpp"
+#include <boost/thread/mutex.hpp>
+#include <boost/shared_ptr.hpp>
+
 namespace fast {
 
 class Streamer : public PipelineObject {
+    public:
+        boost::mutex getStreamMutex();
     private:
         virtual void producerStream() = 0;
 
+        // Mutex used to synchronize the producer running in a secondary thread,
+        // and the consumers working in the main thread
+        boost::mutex streamMutex;
+
 };
 
-}; // end namespace fast
+typedef boost::shared_ptr<Streamer> StreamerPtr;
 
-
-
+} // end namespace fast
 
 #endif /* STREAMER_HPP_ */
