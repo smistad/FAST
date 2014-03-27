223357e5fdda025aec0b2e1207507bfdd862e93e
Erik Smistad <smistad@idi.ntnu.no>
2014-03-27 15:28:45 +0100

    Created the createOpenCLImageFormat function

---
+++ Data/DataTypes.cpp
@@ -0,0 +1,46 @@
+#include "DataTypes.hpp"
+using namespace fast;
+
+cl::ImageFormat createOpenCLImageFormat(DataType type, int components) {
+    cl_channel_order channelOrder;
+    cl_channel_type channelType;
+
+    switch(type) {
+    case TYPE_FLOAT:
+        channelType = CL_FLOAT;
+        break;
+    case TYPE_UINT8:
+        channelType = CL_UNSIGNED_INT8;
+        break;
+    case TYPE_INT8:
+        channelType = CL_SIGNED_INT8;
+        break;
+    case TYPE_UINT16:
+        channelType = CL_UNSIGNED_INT16;
+        break;
+    case TYPE_INT16:
+        channelType = CL_SIGNED_INT16;
+        break;
+    }
+
+    switch(components) {
+    case 1:
+        channelOrder = CL_R;
+        break;
+    case 2:
+        channelOrder = CL_RG;
+        break;
+    case 3:
+        // There is no 3 channel so a 4 channel has to be used
+        channelOrder = CL_RGBA;
+        break;
+    case 4:
+        channelOrder = CL_RGBA;
+        break;
+    default:
+        throw Exception("Number of components has to be between 1 and 4");
+        break;
+    }
+
+    return cl::ImageFormat(channelOrder, channelType);
+}
--- Data/DataTypes.hpp
+++ Data/DataTypes.hpp
@@ -1,9 +1,14 @@
 #ifndef DATA_TYPES_HPP
 #define DATA_TYPES_HPP
 
+#include "Exception.hpp"
+#include "OpenCL.hpp"
+
 namespace fast {
 
 enum DataType { TYPE_FLOAT, TYPE_UINT8, TYPE_INT8, TYPE_UINT16, TYPE_INT16 };
+
+cl::ImageFormat createOpenCLImageFormat(DataType type, int components);
 
 } // end namespace
 
