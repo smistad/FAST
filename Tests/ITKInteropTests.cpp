526dfd9b57625103540e820412db1d7d49edde03
Erik Smistad <smistad@idi.ntnu.no>
2014-04-04 11:32:25 +0200

    created ITK interop tests

---
+++ Tests/ITKInteropTests.cpp
@@ -0,0 +1,24 @@
+#include "catch.hpp"
+#include "ITKImageExporter.hpp"
+#include "ImageImporter2D.hpp"
+
+using namespace fast;
+
+TEST_CASE("Export image to ITK from FAST", "[fast][ITK]") {
+
+    ImageImporter2D::pointer importer = ImageImporter2D::New();
+    importer->setFilename("lena.jpg");
+    Image2D::pointer fastImage = importer->getOutput();
+
+    // ITK Export example
+    typedef itk::Image<float, 2> ImageType;
+    ITKImageExporter<ImageType>::Pointer itkExporter = ITKImageExporter<ImageType>::New();
+    itkExporter->SetInput(fastImage);
+    ImageType::Pointer itkImage = itkExporter->GetOutput();
+    itkExporter->Update();
+
+    ImageType::RegionType region = itkImage->GetLargestPossibleRegion();
+
+    CHECK(fastImage->getWidth() == region.GetSize()[0]);
+    CHECK(fastImage->getHeight() == region.GetSize()[1]);
+}
