f6fde743527d7b67d0b3f098f7ba5c19aa7ab486
Erik Smistad <smistad@idi.ntnu.no>
2014-04-04 15:05:05 +0200

    added support for different data types to the ImageExporter2D

--- Exporters/ImageExporter2D.cpp
+++ Exporters/ImageExporter2D.cpp
@@ -34,16 +34,36 @@ void ImageExporter2D::execute() {
         throw Exception("No filename given to ImageExporter2D");
 
     Image2D::pointer input = mStaticInput;
+    input->update();
 
     QImage image(input->getWidth(), input->getHeight(), QImage::Format_RGB32);
 
     // TODO have to do some type conversion here, assuming float for now
     unsigned char * pixelData = image.bits();
     ImageAccess2D access = input->getImageAccess(ACCESS_READ);
-    float * inputData = (float *)access.get();
+    void * inputData = access.get();
 
     for(unsigned int i = 0; i < input->getWidth()*input->getHeight(); i++) {
-        pixelData[i*4] = round(inputData[i]*255.0f);
+        float data;
+        switch(input->getDataType()) {
+        case TYPE_FLOAT:
+            data = round(((float*)inputData)[i]*255.0f);
+            break;
+        case TYPE_UINT8:
+            data = ((uchar*)inputData)[i];
+            break;
+        case TYPE_INT8:
+            data = ((char*)inputData)[i]+128;
+            break;
+        case TYPE_UINT16:
+            data = ((ushort*)inputData)[i];
+            break;
+        case TYPE_INT16:
+            data = ((short*)inputData)[i];
+            break;
+
+        }
+        pixelData[i*4] = data;
         pixelData[i*4+1] = pixelData[i*4];
         pixelData[i*4+2] = pixelData[i*4];
         pixelData[i*4+3] = 255; // Alpha
