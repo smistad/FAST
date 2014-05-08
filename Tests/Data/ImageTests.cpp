9214a77ee5cf17439a720f006b74d55433e8d12f
Erik Smistad <smistad@idi.ntnu.no>
2014-05-08 17:46:32 +0200

    more tests of Image class

--- Tests/Data/ImageTests.cpp
+++ Tests/Data/ImageTests.cpp
@@ -54,6 +54,92 @@ TEST_CASE("Create a 3D image on an OpenCL device", "[fast][image]") {
     }
 }
 
+void* allocateRandomData(unsigned int nrOfVoxels, DataType type) {
+    srand(time(NULL));
+    switch(type) {
+    case TYPE_FLOAT:
+    {
+        float* data = new float[nrOfVoxels];
+        for(unsigned int i = 0; i < nrOfVoxels; i++)
+            data[i] = (float)rand() / RAND_MAX;
+        return (void*)data;
+    }
+        break;
+    case TYPE_INT8:
+    {
+        char* data = new char[nrOfVoxels];
+        for(unsigned int i = 0; i < nrOfVoxels; i++)
+            data[i] = rand() % 255 - 128;
+        return (void*)data;
+    }
+        break;
+    case TYPE_UINT8:
+    {
+        uchar* data = new uchar[nrOfVoxels];
+        for(unsigned int i = 0; i < nrOfVoxels; i++)
+            data[i] = rand() % 255;
+        return (void*)data;
+    }
+        break;
+    case TYPE_INT16:
+    {
+        short* data = new short[nrOfVoxels];
+        for(unsigned int i = 0; i < nrOfVoxels; i++)
+            data[i] = rand() % 255 - 128;
+        return (void*)data;
+    }
+        break;
+    case TYPE_UINT16:
+    {
+        ushort* data = new ushort[nrOfVoxels];
+        for(unsigned int i = 0; i < nrOfVoxels; i++)
+            data[i] = rand() % 255;
+        return (void*)data;
+    }
+        break;
+    default:
+        return NULL;
+        break;
+    }
+}
+
+bool compareDataArrays(void* data1, void* data2, unsigned int nrOfVoxels, DataType type) {
+    bool success = true;
+    switch(type) {
+        fastSwitchTypeMacro(
+        FAST_TYPE* data1c = (FAST_TYPE*)data1;
+        FAST_TYPE* data2c = (FAST_TYPE*)data2;
+        for(unsigned int i = 0; nrOfVoxels; i++) {
+            if(data1c[i] != data2c[i]) {
+                success = false;
+                break;
+            }
+        }
+        );
+    }
+    return success;
+}
+
+TEST_CASE("Create a 2D image on an OpenCL device with data", "[fast][image]") {
+    DeviceManager& deviceManager = DeviceManager::getInstance();
+    OpenCLDevice::pointer device = deviceManager.getOneOpenCLDevice();
+
+    unsigned int width = 256;
+    unsigned int height = 512;
+    // Test for having components 1 to 4 and for all data types
+    for(unsigned int nrOfComponents = 1; nrOfComponents <= 4; nrOfComponents++) {
+        for(unsigned int typeNr = 0; typeNr < 5; typeNr++) {
+            DataType type = (DataType)typeNr;
+            void* data = allocateRandomData(width*height*nrOfComponents, (DataType)typeNr);
+            Image::pointer image = Image::New();
+            CHECK_NOTHROW(image->create2DImage(width, height, type, nrOfComponents, device, data));
+            ImageAccess access = image->getImageAccess(ACCESS_READ);
+            CHECK(compareDataArrays(data, access.get(), width*height*nrOfComponents, type) == true);
+            deleteArray(data, type);
+        }
+    }
+}
+
 TEST_CASE("Create a 3D image on host", "[fast][image]") {
     Image::pointer image = Image::New();
 
@@ -80,28 +166,14 @@ TEST_CASE("Create a 2D image on host with input data", "[fast][image]") {
     unsigned int height = 512;
 
     // Create a data array with random data
-    uchar* data = new uchar[width*height];
-    srand(time(NULL));
-    for(unsigned int i = 0; i < width*height; i++) {
-        data[i] = rand() % 255;
-    }
+    void* data = allocateRandomData(width*height, TYPE_UINT8);
 
     image->create2DImage(width, height, TYPE_UINT8, 1, Host::New(), data);
 
     ImageAccess access = image->getImageAccess(ACCESS_READ);
-    uchar* returnData = (uchar*)access.get();
+    CHECK(compareDataArrays(data, access.get(), width*height, TYPE_UINT8) == true);
 
-    bool success = true;
-    for(unsigned int i = 0; i < width*height; i++) {
-        if(data[i] != returnData[i]) {
-            success = false;
-            break;
-        }
-    }
-
-    CHECK(success == true);
-
-    delete[] data;
+    deleteArray(data, TYPE_UINT8);
 }
 
 TEST_CASE("Create a 3D image on host with input data", "[fast][image]") {
