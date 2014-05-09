ad574fcc213ab688ba32405fed2a0002be58ad2f
Erik Smistad <smistad@idi.ntnu.no>
2014-05-09 17:09:39 +0200

    created more tests of the image object

--- Tests/Data/ImageTests.cpp
+++ Tests/Data/ImageTests.cpp
@@ -3,6 +3,7 @@
 #include "DeviceManager.hpp"
 #include <ctime>
 #include <cmath>
+#include "HelperFunctions.hpp"
 
 using namespace fast;
 
@@ -136,6 +137,64 @@ bool compareDataArrays(void* data1, void* data2, unsigned int nrOfVoxels, DataTy
     return success;
 }
 
+bool compareBufferWithDataArray(cl::Buffer buffer, OpenCLDevice::pointer device, void* data, unsigned int nrOfVoxels, DataType type) {
+    // First, transfer data from buffer
+    unsigned int elementSize;
+    void* bufferData;
+    switch(type) {
+        fastSwitchTypeMacro(
+            elementSize = sizeof(FAST_TYPE);
+            bufferData = new FAST_TYPE[nrOfVoxels];
+        );
+    }
+    device->getCommandQueue().enqueueReadBuffer(buffer, CL_TRUE, 0, nrOfVoxels*elementSize, bufferData);
+
+    return compareDataArrays(bufferData, data, nrOfVoxels, type);
+}
+
+bool compareImage2DWithDataArray(
+        cl::Image2D image,
+        OpenCLDevice::pointer device,
+        void* data,
+        unsigned int width,
+        unsigned int height,
+        unsigned int nrOfComponents,
+        DataType type) {
+    // First, transfer data from image
+    void* bufferData;
+    unsigned int nrOfVoxels = width*height*nrOfComponents;
+    switch(type) {
+        fastSwitchTypeMacro(
+            bufferData = new FAST_TYPE[nrOfVoxels];
+        );
+    }
+    device->getCommandQueue().enqueueReadImage(image, CL_TRUE, oul::createOrigoRegion(), oul::createRegion(width, height, 1), 0, 0, bufferData);
+
+    return compareDataArrays(bufferData, data, nrOfVoxels, type);
+}
+
+bool compareImage3DWithDataArray(
+        cl::Image3D image,
+        OpenCLDevice::pointer device,
+        void* data,
+        unsigned int width,
+        unsigned int height,
+        unsigned int depth,
+        unsigned int nrOfComponents,
+        DataType type) {
+    // First, transfer data from image
+    void* bufferData;
+    unsigned int nrOfVoxels = width*height*depth*nrOfComponents;
+    switch(type) {
+        fastSwitchTypeMacro(
+            bufferData = new FAST_TYPE[nrOfVoxels];
+        );
+    }
+    device->getCommandQueue().enqueueReadImage(image, CL_TRUE, oul::createOrigoRegion(), oul::createRegion(width, height, depth), 0, 0, bufferData);
+
+    return compareDataArrays(bufferData, data, nrOfVoxels, type);
+}
+
 TEST_CASE("Create a 2D image on an OpenCL device with data", "[fast][image]") {
     DeviceManager& deviceManager = DeviceManager::getInstance();
     OpenCLDevice::pointer device = deviceManager.getOneOpenCLDevice();
@@ -178,8 +237,6 @@ TEST_CASE("Create a 3D image on an OpenCL device with data", "[fast][image]") {
 }
 
 TEST_CASE("Create a 2D image on host with input data", "[fast][image]") {
-
-
     unsigned int width = 256;
     unsigned int height = 512;
 
@@ -220,6 +277,121 @@ TEST_CASE("Create a 3D image on host with input data", "[fast][image]") {
 
             ImageAccess access = image->getImageAccess(ACCESS_READ);
             CHECK(compareDataArrays(data, access.get(), width*height*depth*nrOfComponents, type) == true);
+
+            deleteArray(data, type);
+        }
+    }
+}
+
+TEST_CASE("Create a 2D image on host and request access to OpenCL buffer", "[fast][image]") {
+    DeviceManager& deviceManager = DeviceManager::getInstance();
+    OpenCLDevice::pointer device = deviceManager.getOneOpenCLDevice();
+
+    unsigned int width = 256;
+    unsigned int height = 512;
+
+    // Test for having components 1 to 4 and for all data types
+    for(unsigned int nrOfComponents = 1; nrOfComponents <= 4; nrOfComponents++) {
+        for(unsigned int typeNr = 0; typeNr < 5; typeNr++) {
+            DataType type = (DataType)typeNr;
+
+            // Create a data array with random data
+            void* data = allocateRandomData(width*height*nrOfComponents, type);
+
+            Image::pointer image = Image::New();
+            image->create2DImage(width, height, type, nrOfComponents, Host::New(), data);
+
+            OpenCLBufferAccess access = image->getOpenCLBufferAccess(ACCESS_READ, device);
+            cl::Buffer* buffer = access.get();
+            CHECK(compareBufferWithDataArray(*buffer, device, data, width*height*nrOfComponents, type) == true);
+
+            deleteArray(data, type);
+        }
+    }
+}
+TEST_CASE("Create a 3D image on host and request access to OpenCL buffer", "[fast][image]") {
+    DeviceManager& deviceManager = DeviceManager::getInstance();
+    OpenCLDevice::pointer device = deviceManager.getOneOpenCLDevice();
+
+    unsigned int width = 40;
+    unsigned int height = 64;
+    unsigned int depth = 64;
+
+    // Test for having components 1 to 4 and for all data types
+    for(unsigned int nrOfComponents = 1; nrOfComponents <= 4; nrOfComponents++) {
+        for(unsigned int typeNr = 0; typeNr < 5; typeNr++) {
+            DataType type = (DataType)typeNr;
+
+            // Create a data array with random data
+            void* data = allocateRandomData(width*height*depth*nrOfComponents, type);
+
+            Image::pointer image = Image::New();
+            image->create3DImage(width, height, depth, type, nrOfComponents, Host::New(), data);
+
+            OpenCLBufferAccess access = image->getOpenCLBufferAccess(ACCESS_READ, device);
+            cl::Buffer* buffer = access.get();
+            CHECK(compareBufferWithDataArray(*buffer, device, data, width*height*depth*nrOfComponents, type) == true);
+
+            deleteArray(data, type);
+        }
+    }
+}
+
+TEST_CASE("Create a 2D image on host and request access to OpenCL Image", "[fast][image]") {
+    DeviceManager& deviceManager = DeviceManager::getInstance();
+    OpenCLDevice::pointer device = deviceManager.getOneOpenCLDevice();
+
+    unsigned int width = 256;
+    unsigned int height = 512;
+
+    // Test for having components 1 to 4 and for all data types
+    for(unsigned int nrOfComponents = 1; nrOfComponents <= 4; nrOfComponents++) {
+        // Skip 3 components because an OpenCL image can't have 3 channels
+        if(nrOfComponents == 3)
+            continue;
+        for(unsigned int typeNr = 0; typeNr < 5; typeNr++) {
+            DataType type = (DataType)typeNr;
+
+            // Create a data array with random data
+            void* data = allocateRandomData(width*height*nrOfComponents, type);
+
+            Image::pointer image = Image::New();
+            image->create2DImage(width, height, type, nrOfComponents, Host::New(), data);
+
+            OpenCLImageAccess2D access = image->getOpenCLImageAccess2D(ACCESS_READ, device);
+            cl::Image2D* clImage = access.get();
+            CHECK(compareImage2DWithDataArray(*clImage, device, data, width, height, nrOfComponents, type) == true);
+
+            deleteArray(data, type);
+        }
+    }
+}
+
+TEST_CASE("Create a 3D image on host and request access to OpenCL Image", "[fast][image]") {
+    DeviceManager& deviceManager = DeviceManager::getInstance();
+    OpenCLDevice::pointer device = deviceManager.getOneOpenCLDevice();
+
+    unsigned int width = 40;
+    unsigned int height = 64;
+    unsigned int depth = 64;
+
+    // Test for having components 1 to 4 and for all data types
+    for(unsigned int nrOfComponents = 1; nrOfComponents <= 4; nrOfComponents++) {
+        // Skip 3 components because an OpenCL image can't have 3 channels
+        if(nrOfComponents == 3)
+            continue;
+        for(unsigned int typeNr = 0; typeNr < 5; typeNr++) {
+            DataType type = (DataType)typeNr;
+
+            // Create a data array with random data
+            void* data = allocateRandomData(width*height*depth*nrOfComponents, type);
+
+            Image::pointer image = Image::New();
+            image->create3DImage(width, height, depth, type, nrOfComponents, Host::New(), data);
+
+            OpenCLImageAccess3D access = image->getOpenCLImageAccess3D(ACCESS_READ, device);
+            cl::Image3D* clImage = access.get();
+            CHECK(compareImage3DWithDataArray(*clImage, device, data, width, height, depth, nrOfComponents, type) == true);
 
             deleteArray(data, type);
         }
