5dfe939b511dd118e531ded722a0ee09b56a31b1
Erik Smistad <smistad@idi.ntnu.no>
2014-05-15 14:28:53 +0200

    added a simple vector type

--- Data/DataTypes.hpp
+++ Data/DataTypes.hpp
@@ -30,6 +30,75 @@ float getDefaultIntensityWindow(DataType type);
 
 void deleteArray(void * data, DataType type);
 
+template <class T, int N>
+class Vector {
+    public:
+        T x();
+        T y();
+        T z();
+        T& operator[](unsigned int index); // lvalue
+        const T& operator[](unsigned int index) const; // rvalue
+        int getSize() const;
+        Vector(const Vector& other);
+        Vector();
+        ~Vector();
+    protected:
+        T* data;
+};
+
+template <int N>
+class Float : public Vector<float, N> {
+};
+
 } // end namespace
+
+template<class T, int N>
+inline T fast::Vector<T,N>::x() {
+    return data[0];
+}
+
+template<class T, int N>
+inline T fast::Vector<T,N>::y() {
+    return data[1];
+}
+
+template<class T, int N>
+inline T fast::Vector<T,N>::z() {
+    return data[2];
+}
+
+template<class T, int N>
+inline T& fast::Vector<T,N>::operator [](unsigned int index) {
+    return data[index];
+}
+
+template<class T, int N>
+inline const T& fast::Vector<T,N>::operator [](unsigned int index) const {
+    return const_cast<T&>((*this)[index]);
+}
+
+template<class T, int N>
+inline int fast::Vector<T,N>::getSize() const {
+    return N;
+}
+
+template<class T, int N>
+inline fast::Vector<T,N>::Vector(const Vector& other) {
+    data = new T[N];
+    for(unsigned int i = 0; i < N; i++)
+        data[i] = other[i];
+}
+
+template<class T, int N>
+inline fast::Vector<T,N>::Vector() {
+    data = new T[N];
+    for(unsigned int i = 0; i < N; i++)
+        data[i] = 0;
+}
+
+template<class T, int N>
+inline fast::Vector<T,N>::~Vector() {
+    delete[] data;
+}
 
 #endif
