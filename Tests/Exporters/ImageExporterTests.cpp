9a1082687c3125e5fab84954301aabe0eb0f9ea8
Erik Smistad <smistad@idi.ntnu.no>
2014-05-20 12:47:58 +0200

    wrote a tests for the ImageExporter

--- Tests/Exporters/ImageExporterTests.cpp
+++ Tests/Exporters/ImageExporterTests.cpp
@@ -1,6 +1,8 @@
 #include "catch.hpp"
 #include "ImageExporter.hpp"
+#include "ImageImporter.hpp"
 #include "Image.hpp"
+#include "DataComparison.hpp"
 
 using namespace fast;
 
@@ -15,5 +17,30 @@ TEST_CASE("No input given to the ImageExporter", "[fast][ImageExporter]") {
     ImageExporter::pointer exporter = ImageExporter::New();
     exporter->setFilename("asd");
     CHECK_THROWS(exporter->update());
+}
+
+TEST_CASE("Write 2D image with the ImageExporter", "[fast][ImageExporter]") {
+
+    unsigned int width = 32;
+    unsigned int height = 46;
+    unsigned int components = 1;
+    DataType type = TYPE_UINT8;
+
+    Image::pointer image = Image::New();
+    image->create2DImage(width,height,type,components,Host::New());
+
+    ImageExporter::pointer exporter = ImageExporter::New();
+    exporter->setFilename("ImageExporterTest.jpg");
+    exporter->setInput(image);
+    exporter->update();
+
+    ImageImporter::pointer importer = ImageImporter::New();
+    importer->setFilename("ImageExporterTest.jpg");
+    Image::pointer image2 = importer->getOutput();
+    importer->update();
+
+    CHECK(width == image2->getWidth());
+    CHECK(height == image2->getHeight());
+
 }
 
