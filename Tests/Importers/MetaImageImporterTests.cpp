8db298b6c79a651273fa09bf4fdde87648e638b5
Erik Smistad <smistad@idi.ntnu.no>
2014-05-30 21:27:06 +0200

    added a 2D tests to the meta image importer

--- Tests/Importers/MetaImageImporterTests.cpp
+++ Tests/Importers/MetaImageImporterTests.cpp
@@ -15,7 +15,40 @@ TEST_CASE("Import non-existing file to MetaImageImporter", "[fast][MetaImageImpo
     CHECK_THROWS_AS(importer->update(), FileNotFoundException);
 }
 
-TEST_CASE("Import MetaImage file to host", "[fast][MetaImageImporter]") {
+TEST_CASE("Import 2D MetaImage file to host", "[fast][MetaImageImporter]") {
+    MetaImageImporter::pointer importer = MetaImageImporter::New();
+    importer->setFilename(std::string(FAST_TEST_DATA_DIR)+"US-2Dt/US-2Dt_0.mhd");
+    importer->setDevice(Host::New());
+    Image::pointer image = importer->getOutput();
+    image->update();
+
+    // Check attributes of image
+    CHECK(image->getWidth() == 471);
+    CHECK(image->getHeight() == 616);
+    CHECK(image->getDepth() == 1);
+    CHECK(image->getDimensions() == 2);
+    CHECK(image->getSpacing().x() == Approx(0.081));
+    CHECK(image->getSpacing().y() == Approx(0.081));
+    CHECK(image->getSpacing().z() == Approx(1.0));
+    CHECK(image->getOffset().x() == Approx(32.2747));
+    CHECK(image->getOffset().y() == Approx(-2.02993));
+    CHECK(image->getOffset().z() == Approx(170.997));
+    CHECK(image->getCenterOfRotation().x() == Approx(0));
+    CHECK(image->getCenterOfRotation().y() == Approx(0));
+    CHECK(image->getCenterOfRotation().z() == Approx(0));
+    CHECK(image->getTransformMatrix()[0] == Approx(-0.963423));
+    CHECK(image->getTransformMatrix()[1] == Approx(-0.263617));
+    CHECK(image->getTransformMatrix()[2] == Approx(-0.0481956));
+    CHECK(image->getTransformMatrix()[3] == Approx(0.0225832));
+    CHECK(image->getTransformMatrix()[4] == Approx(0.0993386));
+    CHECK(image->getTransformMatrix()[5] == Approx(-0.994797));
+    CHECK(image->getTransformMatrix()[6] == Approx(0.267034));
+    CHECK(image->getTransformMatrix()[7] == Approx(-0.959499));
+    CHECK(image->getTransformMatrix()[8] == Approx(-0.089752));
+    CHECK(image->getDataType() == TYPE_UINT8);
+}
+
+TEST_CASE("Import 3D MetaImage file to host", "[fast][MetaImageImporter]") {
     MetaImageImporter::pointer importer = MetaImageImporter::New();
     importer->setFilename(std::string(FAST_TEST_DATA_DIR)+"US-3Dt/US-3Dt_0.mhd");
     importer->setDevice(Host::New());
