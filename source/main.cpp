00a3afaa4f1de244a19945e90fe7548abddc0f97
Erik Smistad <smistad@idi.ntnu.no>
2014-11-26 13:24:37 +0100

    reverted main

--- source/main.cpp
+++ source/main.cpp
@@ -15,129 +15,212 @@
 #include "ColorTransferFunction.hpp"
 #include "OpacityTransferFunction.hpp"
 #include "SurfaceExtraction.hpp"
-#include "VTKSurfaceFileImporter.hpp"
-#include "VesselDetection.hpp"
-#include "IterativeClosestPoint.hpp"
-#include "VTKPointSetFileImporter.hpp"
-#include "SceneGraph.hpp"
-#include "PointRenderer.hpp"
-#include "Color.hpp"
-#include "ImageFileStreamer.hpp"
-#include "MeshRenderer.hpp"
 
 using namespace fast;
 
 int main(int argc, char ** argv) {
-    MetaImageImporter::pointer CTimporter = MetaImageImporter::New();
-    CTimporter->setFilename(std::string(FAST_TEST_DATA_DIR) + "CT-Abdomen.mhd");
-    Image::pointer CTimage = CTimporter->getOutput();
 
-    VTKSurfaceFileImporter::pointer arteryImporter = VTKSurfaceFileImporter::New();
-    arteryImporter->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/artery.vtk");
-    Mesh::pointer artery = arteryImporter->getOutput();
+    // TODO this causes problem for some reason??
+    // Get a GPU device and set it as the default device
+    //DeviceManager& deviceManager = DeviceManager::getInstance();
+    //deviceManager.setDefaultDevice(deviceManager.getOneGPUDevice(true));
 
-    VTKSurfaceFileImporter::pointer boneImporter = VTKSurfaceFileImporter::New();
-    boneImporter->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/bones.vtk");
-    Mesh::pointer bones = boneImporter->getOutput();
+    /*
+    MetaImageImporter::pointer importer = MetaImageImporter::New();
+    importer->setFilename(std::string(FAST_ROOT_DIR)+"TestData/US-3Dt/US-3Dt_0.mhd");
+    Image::pointer image = importer->getOutput();
+    //ImageFileStreamer::pointer importer = ImageFileStreamer::New();
+    //importer->setFilenameFormat(std::string(FAST_ROOT_DIR)+"TestData/US-3Dt/US-3Dt_#.mhd");
+    //DynamicImage::pointer image = importer->getOutput();
+    SurfaceExtraction::pointer extractor = SurfaceExtraction::New();
+    extractor->setInput(image);
+    extractor->setThreshold(150);
+    Surface::pointer surface = extractor->getOutput();
+    //extractor->update();
 
-    MetaImageImporter::pointer importer1 = MetaImageImporter::New();
-    importer1->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/US-Acq_05_20141021T102120_Sonix_0.mhd");
-    Image::pointer image1 = importer1->getOutput();
-
-    MetaImageImporter::pointer importer2 = MetaImageImporter::New();
-    importer2->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/US-Acq_05_20141021T102120_Sonix_40.mhd");
-    Image::pointer image2 = importer2->getOutput();
-
-    MetaImageImporter::pointer importer3 = MetaImageImporter::New();
-    importer3->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/US-Acq_05_20141021T102120_Sonix_120.mhd");
-    Image::pointer image3 = importer3->getOutput();
-
-    MetaImageImporter::pointer importer4 = MetaImageImporter::New();
-    importer4->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/US-Acq_05_20141021T102120_Sonix_20.mhd");
-    Image::pointer image4 = importer4->getOutput();
-
-    MetaImageImporter::pointer importer5 = MetaImageImporter::New();
-    importer5->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/US-Acq_05_20141021T102120_Sonix_60.mhd");
-    Image::pointer image5 = importer5->getOutput();
-
-    MetaImageImporter::pointer importer6 = MetaImageImporter::New();
-    importer6->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/US-Acq_05_20141021T102120_Sonix_100.mhd");
-    Image::pointer image6 = importer6->getOutput();
-
-    SliceRenderer::pointer CTrenderer = SliceRenderer::New();
-    CTrenderer->setInput(CTimage);
-    CTrenderer->setSlicePlane(PLANE_Z);
-    CTrenderer->setSliceToRender(15);
-    MeshRenderer::pointer arteryRenderer = MeshRenderer::New();
-    arteryRenderer->setInput(artery);
-    MeshRenderer::pointer bonesRenderer = MeshRenderer::New();
-    bonesRenderer->setInput(bones);
-    ImageRenderer::pointer renderer = ImageRenderer::New();
-    renderer->setInput(image1);
-    ImageRenderer::pointer renderer2 = ImageRenderer::New();
-    renderer2->setInput(image2);
-    ImageRenderer::pointer renderer3 = ImageRenderer::New();
-    renderer3->setInput(image3);
-    ImageRenderer::pointer renderer4 = ImageRenderer::New();
-    renderer4->setInput(image4);
-    ImageRenderer::pointer renderer5 = ImageRenderer::New();
-    renderer5->setInput(image5);
-    ImageRenderer::pointer renderer6 = ImageRenderer::New();
-    renderer6->setInput(image6);
+    MeshRenderer::pointer surfaceRenderer = MeshRenderer::New();
+    surfaceRenderer->setInput(surface);
+    SliceRenderer::pointer sliceRenderer = SliceRenderer::New();
+    sliceRenderer->setInput(image);
+    SliceRenderer::pointer sliceRenderer2 = SliceRenderer::New();
+    sliceRenderer2->setInput(image);
+    sliceRenderer2->setSlicePlane(PLANE_X);
+    //sliceRenderer2->setSliceToRender(200);
     SimpleWindow::pointer window = SimpleWindow::New();
-    /*
-    window->addRenderer(renderer);
-    window->addRenderer(renderer2);
-    window->addRenderer(renderer3);
-    window->addRenderer(renderer4);
-    window->addRenderer(renderer5);
-    window->addRenderer(renderer6);
-    //window->addRenderer(CTrenderer);
-    window->addRenderer(arteryRenderer);
-    //window->addRenderer(bonesRenderer);
-     */
+    window->addRenderer(surfaceRenderer);
+    window->addRenderer(sliceRenderer);
+    window->addRenderer(sliceRenderer2);
+    window->runMainLoop();
+    */
+	/*
+    MetaImageImporter::pointer importer = MetaImageImporter::New();
+    importer->setFilename(std::string(FAST_ROOT_DIR)+"TestData/US-3Dt/US-3Dt_0.mhd");
+    SliceRenderer::pointer renderer = SliceRenderer::New();
+    renderer->setSlicePlane(PLANE_Y);
+    renderer->setInput(importer->getOutput());
+    SimpleWindow::pointer window = SimpleWindow::New();
 
-    VesselDetection::pointer vesselDetection = VesselDetection::New();
-    vesselDetection->addImage(image1);
+    window->addRenderer(renderer);
+    window->runMainLoop();
+	*/
+	/*
+
+    ImageImporter::pointer importer2 = ImageImporter::New();
+    importer2->setFilename(std::string(FAST_ROOT_DIR)+"TestData/US-2D.jpg");
+    ImageRenderer::pointer renderer2 = ImageRenderer::New();
+    renderer2->setInput(importer2->getOutput());
+    SimpleWindow::pointer window2 = SimpleWindow::New();
+    window2->set2DMode();
+    window2->addRenderer(renderer2);
+    window2->runMainLoop();
+	*/
+
+
     /*
-    vesselDetection->addImage(image2);
-    vesselDetection->addImage(image3);
-    vesselDetection->addImage(image4);
-    vesselDetection->addImage(image5);
-    vesselDetection->addImage(image6);
+    ImageFileStreamer::pointer importer = ImageFileStreamer::New();
+    importer->setFilenameFormat(std::string(FAST_ROOT_DIR)+"TestData/US-3Dt/US-3Dt_#.mhd");
+    SurfaceExtraction::pointer extractor = SurfaceExtraction::New();
+    extractor->setInput(importer->getOutput());
+    extractor->setThreshold(200);
+    Surface::pointer surface = extractor->getOutput();
+    extractor->update();
+
+    MeshRenderer::pointer surfaceRenderer = MeshRenderer::New();
+    surfaceRenderer->setInput(surface);
+    SimpleWindow::pointer window = SimpleWindow::New();
+    window->addRenderer(surfaceRenderer);
+    window->runMainLoop();
     */
 
-    Image::pointer image_circle = vesselDetection->getOutput();
-    PointSet::pointer points = vesselDetection->getPointSet();
     /*
+    // Example of importing, processing and exporting a 2D image
+    ImageImporter::pointer importer = ImageImporter::New();
+    importer->setFilename(std::string(FAST_ROOT_DIR)+"TestData/US-2D.jpg");
+    GaussianSmoothingFilter::pointer filter = GaussianSmoothingFilter::New();
+    filter->setInput(importer->getOutput());
+    filter->setMaskSize(7);
+    filter->setStandardDeviation(10);
+    Image::pointer filteredImage = filter->getOutput();
 
-    VTKPointSetFileImporter::pointer pointSetImporter = VTKPointSetFileImporter::New();
-    pointSetImporter->setFilename("/home/smistad/Dropbox/RASimAs/VesselDetection/artery.vtk");
-    PointSet::pointer modelPoints = pointSetImporter->getOutput();
+    ImageExporter::pointer exporter = ImageExporter::New();
+    exporter->setFilename("test.jpg");
+    exporter->setInput(filteredImage);
+    exporter->update();
+    MetaImageExporter::pointer exporter2 = MetaImageExporter::New();
+    exporter2->setFilename("test.mhd");
+    exporter2->setInput(filteredImage);
+    exporter2->update();
+	*/
+    // Example of displaying an image on screen using ImageRenderer (2D) and SimpleWindow
 
-    IterativeClosestPoint::pointer icp = IterativeClosestPoint::New();
-    icp->setFixedPointSet(points);
-    icp->setMovingPointSet(modelPoints);
-    icp->setTransformationType(IterativeClosestPoint::TRANSLATION);
-    icp->update();
-
-    PointRenderer::pointer pointRenderer = PointRenderer::New();
-    pointRenderer->addInput(points);
-    //window->addRenderer(pointRenderer);
-
-    // Get transform and apply it to the model
-    artery->update();
-    SceneGraph& graph = SceneGraph::getInstance();
-    SceneGraphNode::pointer node = graph.getDataNode(artery);
-    node->setTransformation(icp->getOutputTransformation());
-    //bones->update();
-    //SceneGraphNode::pointer node2 = graph.getDataNode(bones);
-    //node2->setTransformation(icp->getOutputTransformation());
-    //window->runMainLoop();
-*/
-    // Show vessel detection result
-    ImageRenderer::pointer rendererCircle = ImageRenderer::New();
-    rendererCircle->setInput(image_circle);
-    window->addRenderer(rendererCircle);
+/*
+    SimpleWindow::pointer window = SimpleWindow::New();
+    ImageRenderer::pointer renderer = ImageRenderer::New();
+    renderer->setInput(filteredImage);
+    window->addRenderer(renderer);
+window->setTimeout(10*1000);
     window->runMainLoop();
+    */
+    
+	
+/*
+    ImageFileStreamer::pointer mhdStreamer = ImageFileStreamer::New();
+    mhdStreamer->setFilenameFormat(std::string(FAST_ROOT_DIR)+"TestData/US-3Dt/US-3Dt_#.mhd");
+    MeshRenderer::pointer surfaceRenderer = MeshRenderer::New();
+    surfaceRenderer->setInput(mhdStreamer->getOutput());
+    surfaceRenderer->setThreshold(200);
+    SimpleWindow::pointer window = SimpleWindow::New();
+    window->setMaximumFramerate(25);
+    window->addRenderer(surfaceRenderer);
+    window->runMainLoop();
+*/
+	// Example of using VolumeRenderer (3D) and SimpleWindow
+
+
+//	MetaImageImporter::pointer mhdImporter = MetaImageImporter::New();
+//    mhdImporter->setFilename("skull.mhd");
+	
+
+	ImageFileStreamer::pointer mhdStreamer = ImageFileStreamer::New();
+    mhdStreamer->setFilenameFormat(std::string(FAST_ROOT_DIR)+"TestData/US-3Dt/US-3Dt_#.mhd");
+
+    SliceRenderer::pointer sRenderer = SliceRenderer::New();
+    sRenderer->setSlicePlane(PLANE_Y);
+    sRenderer->setInput(mhdStreamer->getOutput());
+	sRenderer->setSliceToRender(120);
+
+
+
+
+    //SimpleWindow::pointer window = SimpleWindow::New();
+	//window->addRenderer(sRenderer);
+    //window->runMainLoop();
+	
+
+	
+
+
+	MetaImageImporter::pointer mhdImporter = MetaImageImporter::New();
+    mhdImporter->setFilename(std::string(FAST_ROOT_DIR)+"TestData/skull256.mhd");
+
+	MetaImageImporter::pointer mhdImporter2 = MetaImageImporter::New();
+	mhdImporter2->setFilename(std::string(FAST_ROOT_DIR) + "TestData/v8.mhd");
+	
+	
+	ColorTransferFunction::pointer ctf1 = ColorTransferFunction::New();
+	ctf1->addRGBPoint(000.0, 1.0, 0.0, 0.0);
+	ctf1->addRGBPoint(127.0, 0.0, 1.0, 0.0);
+	ctf1->addRGBPoint(255.0, 0.0, 0.0, 1.0);
+	ColorTransferFunction::pointer ctf2 = ColorTransferFunction::New();
+	ctf2->addRGBPoint(000.0, 1.0, 0.0, 0.0);
+	ctf2->addRGBPoint(127.0, 0.0, 1.0, 0.0);
+	ctf2->addRGBPoint(255.0, 0.0, 0.0, 1.0);
+
+
+	OpacityTransferFunction::pointer otf1 = OpacityTransferFunction::New();
+	otf1->addAlphaPoint(000.0, 0.0);
+	otf1->addAlphaPoint(200.0, 1.0);
+	otf1->addAlphaPoint(255.0, 1.0);
+	
+	OpacityTransferFunction::pointer otf2 = OpacityTransferFunction::New();
+	otf2->addAlphaPoint(000.0, 0.0);
+	otf2->addAlphaPoint(255.0, 1.0);
+
+
+
+	VolumeRenderer::pointer vRenderer = VolumeRenderer::New();
+    //VolumeRenderer->addInput(mhdImporter->getOutput());
+	vRenderer->addInput(mhdStreamer->getOutput());
+	vRenderer->addInput(mhdStreamer->getOutput());
+	vRenderer->addInput(mhdStreamer->getOutput());
+	//vRenderer->addInput(mhdStreamer->getOutput());
+	//vRenderer->addInput(mhdImporter2->getOutput());
+	//vRenderer->addInput(mhdImporter->getOutput());
+	//vRenderer->turnOffTransformations();
+	
+	vRenderer->setColorTransferFunction(0, ctf1);
+	vRenderer->setColorTransferFunction(1, ctf2);
+	vRenderer->setColorTransferFunction(2, ctf1);
+
+	vRenderer->setOpacityTransferFunction(0, otf1);
+	vRenderer->setOpacityTransferFunction(1, otf2);
+	vRenderer->setOpacityTransferFunction(2, otf1);
+	float ut[16]={	1.0, 0.0, 0.0, -100.0,
+					0.0, 1.0, 0.0, 0.0,
+					0.0, 0.0, 1.0, 0.0,
+					0.0, 0.0, 0.0, 1.0};
+	vRenderer->setUserTransform(1, ut);
+	ut[3] = 100.0;
+	vRenderer->setUserTransform(2, ut);
+    vRenderer->enableRuntimeMeasurements();
+	SimpleWindow::pointer window = SimpleWindow::New();
+    window->setMaximumFramerate(200);
+    window->addRenderer(vRenderer);
+	window->addRenderer(sRenderer);
+    window->runMainLoop();
+	vRenderer->getRuntime()->print();
+	
+	getchar();
+
+
 }
