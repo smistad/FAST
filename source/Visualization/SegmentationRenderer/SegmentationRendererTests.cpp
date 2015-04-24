32f2a46c410d847008a7681623327e7a9f8a94e9
Erik Smistad <smistad@idi.ntnu.no>
2015-04-24 13:23:42 +0200

    added a segmentation renderer test for changing color

--- source/Visualization/SegmentationRenderer/SegmentationRendererTests.cpp
+++ source/Visualization/SegmentationRenderer/SegmentationRendererTests.cpp
@@ -76,3 +76,27 @@ TEST_CASE("SegmentationRenderer on a stream of thresholded 2D images with draw c
     window->start();
 }
 
+TEST_CASE("SegmentationRenderer on a stream of thresholded 2D images and set different color", "[fast][SegmentationRenderer][visual]") {
+    ImageFileStreamer::pointer importer = ImageFileStreamer::New();
+    importer->setFilenameFormat(std::string(FAST_TEST_DATA_DIR) + "US-2Dt/US-2Dt_#.mhd");
+
+    BinaryThresholding::pointer segmentation = BinaryThresholding::New();
+    segmentation->setInputConnection(importer->getOutputPort());
+    segmentation->setLowerThreshold(100);
+
+    ImageRenderer::pointer imageRenderer = ImageRenderer::New();
+    imageRenderer->addInputConnection(importer->getOutputPort());
+
+    SegmentationRenderer::pointer renderer = SegmentationRenderer::New();
+    renderer->addInputConnection(segmentation->getOutputPort());
+    renderer->setColor(Segmentation::LABEL_FOREGROUND, Color::Red());
+
+    SimpleWindow::pointer window = SimpleWindow::New();
+    window->set2DMode();
+    window->addRenderer(imageRenderer);
+    window->addRenderer(renderer);
+    window->setTimeout(2000);
+    window->start();
+}
+
+
