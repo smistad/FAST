496447cb566faee31a48cf1c57f6a046013fbf9b
Erik Smistad <smistad@idi.ntnu.no>
2015-06-09 15:08:02 +0200

    created the bounding box renderer

---
+++ source/FAST/Visualization/BoundingBoxRenderer/BoundingBoxRendererTests.cpp
@@ -0,0 +1,19 @@
+#include "FAST/Tests/catch.hpp"
+#include "BoundingBoxRenderer.hpp"
+#include "FAST/Importers/VTKMeshFileImporter.hpp"
+#include "FAST/Visualization/SimpleWindow.hpp"
+
+using namespace fast;
+
+TEST_CASE("BoundingBox renderer", "[BoundingBoxRenderer][fast]") {
+    CHECK_NOTHROW(
+        VTKMeshFileImporter::pointer importer = VTKMeshFileImporter::New();
+        importer->setFilename(std::string(FAST_TEST_DATA_DIR) + "Surface_LV.vtk");
+        BoundingBoxRenderer::pointer renderer = BoundingBoxRenderer::New();
+        renderer->addInputConnection(importer->getOutputPort());
+        SimpleWindow::pointer window = SimpleWindow::New();
+        window->addRenderer(renderer);
+        window->setTimeout(1000);
+        window->start();
+    );
+}
