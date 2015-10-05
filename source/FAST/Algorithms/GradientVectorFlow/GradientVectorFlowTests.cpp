f1720074e3e37baabe09db3662e4a2267a083c02
Erik Smistad <smistad@idi.ntnu.no>
2015-10-05 10:05:51 +0200

    fixed a compile error in test

--- source/FAST/Algorithms/GradientVectorFlow/GradientVectorFlowTests.cpp
+++ source/FAST/Algorithms/GradientVectorFlow/GradientVectorFlowTests.cpp
@@ -36,7 +36,7 @@ Vector3f calculateLaplacian(ImageAccess::pointer& input, ImageAccess::pointer& r
 double calculateGVFVectorFieldResidual(Image::pointer inputVectorField, Image::pointer vectorField, float mu) {
     ImageAccess::pointer input = inputVectorField->getImageAccess(ACCESS_READ);
     ImageAccess::pointer result = vectorField->getImageAccess(ACCESS_READ);
-    reportInfo() << "Calculating residual..." << Reporter::end;
+    Reporter::info() << "Calculating residual..." << Reporter::end;
 
     double sum = 0.0;
     uint size;
