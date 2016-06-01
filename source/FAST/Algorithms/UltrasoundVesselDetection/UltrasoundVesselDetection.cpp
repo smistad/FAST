68f7157f56faa68c3f191c80b4c0b45c8212ef37
Erik Smistad <ersmistad@gmail.com>
2016-06-01 09:21:49 +0200

    Updated model files

--- source/FAST/Algorithms/UltrasoundVesselDetection/UltrasoundVesselDetection.cpp
+++ source/FAST/Algorithms/UltrasoundVesselDetection/UltrasoundVesselDetection.cpp
@@ -38,9 +38,9 @@ UltrasoundVesselDetection::UltrasoundVesselDetection() {
 	//std::string modelFile = "/home/smistad/vessel_detection_model_alex_no_relu/deploy.prototxt";
 	//std::string trainingFile = "/home/smistad/vessel_detection_model_alex_no_relu/snapshot_iter_630.caffemodel";
 	//std::string meanFile = "/home/smistad/vessel_detection_model_alex_no_relu/mean.binaryproto";
-	std::string modelFile = "/home/smistad/vessel_detection_model_alex_augmented/deploy.prototxt";
-	std::string trainingFile = "/home/smistad/vessel_detection_model_alex_augmented/snapshot_iter_12040.caffemodel";
-	std::string meanFile = "/home/smistad/vessel_detection_model_alex_augmented/mean.binaryproto";
+	std::string modelFile = "/home/smistad/vessel_net/deploy.prototxt";
+	std::string trainingFile = "/home/smistad/vessel_net/snapshot_iter_7350.caffemodel";
+	std::string meanFile = "/home/smistad/vessel_net/mean.binaryproto";
 	mClassifier->loadModel(modelFile, trainingFile, meanFile);
 
 }
