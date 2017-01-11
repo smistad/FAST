3574ac5fb2a564e4d02f07a4bf07f2a6d90ef433
Erik Smistad <ersmistad@gmail.com>
2017-01-11 13:56:44 +0100

    Added external project for glew

---
+++ cmake/ExternalGLEW.cmake
@@ -0,0 +1,20 @@
+# Download and set up GLEW
+
+include(cmake/Externals.cmake)
+
+ExternalProject_Add(glew
+        PREFIX ${FAST_EXTERNAL_BUILD_DIR}/glew
+        BINARY_DIR ${FAST_EXTERNAL_BUILD_DIR}/glew
+        # The original GLEW library of https://github.com/nigels-com/glew/ has not proper cmake setup
+        # Use this repo instead
+        GIT_REPOSITORY "https://github.com/Perlmint/glew-cmake.git"
+        GIT_TAG "glew-cmake-1.11.0"
+        CMAKE_CACHE_ARGS
+        -DCMAKE_BUILD_TYPE:STRING=Release
+        -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
+        -DCMAKE_INSTALL_PREFIX:STRING=${FAST_EXTERNAL_INSTALL_DIR}
+)
+
+set(GLEW_LIBRARY ${FAST_EXTERNAL_INSTALL_DIR}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}glew${CMAKE_SHARED_LIBRARY_SUFFIX})
+list(APPEND LIBRARIES ${GLEW_LIBRARY})
+list(APPEND FAST_EXTERNAL_DEPENDENCIES glew)
