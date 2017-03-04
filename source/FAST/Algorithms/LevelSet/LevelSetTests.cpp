6f0aa04de58b05940e81a7e0a8d593a3e4f2ad40
Erik Smistad <ersmistad@gmail.com>
2017-03-04 19:33:20 +0100

    Started on level set implementation

---
+++ source/FAST/Algorithms/LevelSet/LevelSetTests.cpp
@@ -0,0 +1,35 @@
+#include "FAST/Testing.hpp"
+#include "LevelSetSegmentation.hpp"
+#include "FAST/Importers/ImageFileImporter.hpp"
+#include "FAST/Algorithms/SurfaceExtraction/SurfaceExtraction.hpp"
+#include "FAST/Visualization/MeshRenderer/MeshRenderer.hpp"
+#include "FAST/Visualization/SimpleWindow.hpp"
+
+using namespace fast;
+
+/*
+TEST_CASE("Level set segmentation", "[fast][levelset][visual]") {
+    ImageFileImporter::pointer importer = ImageFileImporter::New();
+    importer->setFilename(Config::getTestDataPath() + "CT/CT-Abdomen.mhd");
+
+    LevelSetSegmentation::pointer segmentation = LevelSetSegmentation::New();
+    segmentation->setIntensityMean(150);
+    segmentation->setIntensityVariance(50);
+    segmentation->setCurvatureWeight(0.99);
+    segmentation->setMaxIterations(5000);
+    segmentation->addSeedPoint(Vector3i(149, 210, 345), 10);
+    segmentation->setInputConnection(importer->getOutputPort());
+    segmentation->update();
+
+    SurfaceExtraction::pointer extraction = SurfaceExtraction::New();
+    extraction->setInputConnection(segmentation->getOutputPort());
+
+    MeshRenderer::pointer renderer = MeshRenderer::New();
+    renderer->addInputConnection(extraction->getOutputPort());
+
+    SimpleWindow::pointer window = SimpleWindow::New();
+    window->addRenderer(renderer);
+    window->set3DMode();
+    window->start();
+}
+ */
