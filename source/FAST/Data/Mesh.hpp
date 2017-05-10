b71401f8f6740f010690fc5f59b378e26b259e91
Erik Smistad <ersmistad@gmail.com>
2017-05-10 11:14:59 +0200

    Improved documentation

--- source/FAST/Data/Mesh.hpp
+++ source/FAST/Data/Mesh.hpp
@@ -15,8 +15,8 @@
 namespace fast {
 
 /**
- * The mesh data object contains vertices and optionally a set of lines and/or triangles.
- * Each vertex is represented as a MeshVertex and the lines and triangles as MeshLine and MeshTriangle respectively.
+ * \brief The mesh data object contains vertices and optionally a set of lines and/or triangles.
+ *      Each vertex is represented as a MeshVertex and the lines and triangles as MeshLine and MeshTriangle respectively.
  */
 class Mesh : public SpatialDataObject {
     FAST_OBJECT(Mesh)
