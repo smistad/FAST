fc7c1db6b6e1b582690e6b06d75514e0b09440d5
Erik Smistad <ersmistad@gmail.com>
2017-09-19 14:51:52 +0200

    Temporal averaging on classification to text PO

--- source/FAST/Algorithms/NeuralNetwork/ImageClassifier.hpp
+++ source/FAST/Algorithms/NeuralNetwork/ImageClassifier.hpp
@@ -13,7 +13,9 @@ namespace fast {
 typedef std::map<std::string, float> classifications;
 FAST_SIMPLE_DATA_OBJECT(ImageClassification, classifications)
 
-
+/**
+ * Neural network image classification
+ */
 class FAST_EXPORT  ImageClassifier : public NeuralNetwork {
 	FAST_OBJECT(ImageClassifier)
 	public:
@@ -31,38 +33,67 @@ class FAST_EXPORT  ImageClassifier : public NeuralNetwork {
 };
 
 
-/*
+/**
  * ProcessObject to convert a classification into text
  */
 class FAST_EXPORT  ClassificationToText : public ProcessObject {
     FAST_OBJECT(ClassificationToText)
-private:
-    ClassificationToText() {
-        createInputPort<ImageClassification>(0);
-        createOutputPort<Text>(0);
-    }
-    void execute() {
-        ImageClassification::pointer classification = getInputData<ImageClassification>();
-        Text::pointer text = getOutputData<Text>();
-
-        // Find classification with max
-        ImageClassification::access access = classification->getAccess(ACCESS_READ);
-        std::map<std::string, float> values = access->getData();
-        float max = 0;
-        std::string label;
-        for (auto &&item : values) {
-            if(item.second > max) {
-                max = item.second;
-                label = item.first;
-            }
+    private:
+        std::deque<std::map<std::string, float>> mBuffer; // used for calculating temporal average
+        int mBufferSize = 100; // How large the buffer can be
+        ClassificationToText() {
+            createInputPort<ImageClassification>(0);
+            createOutputPort<Text>(0);
+            createIntegerAttribute("average_size", "Average size", "nr of frames to average", 100);
         }
+        void loadAttributes(std::vector<Attribute> attributes) {
+            mBufferSize = getIntegerAttribute("avarage_size");
+        }
+        void execute() {
+            ImageClassification::pointer classification = getInputData<ImageClassification>();
+            Text::pointer text = getOutputData<Text>();
 
-        Text::access access2 = text->getAccess(ACCESS_READ_WRITE);
-        char buffer[8];
-        std::sprintf(buffer, "%.2f", max);
-        std::string result = label + ": " + buffer;
-        access2->setData(result);
-    }
+            ImageClassification::access access = classification->getAccess(ACCESS_READ);
+            std::map<std::string, float> values = access->getData();
+
+            // Add to buffer
+            mBuffer.push_back(values);
+
+            // Remove item from buffer
+            if(mBuffer.size() > mBufferSize)
+                mBuffer.pop_front();
+
+            // Calculate average
+            for(auto&& val : values)
+               val.second = 0;
+
+            // Sum
+            for(auto sample : mBuffer) {
+                for(auto val : sample) {
+                    values[val.first] += val.second;
+                }
+            }
+            // Divide
+            for(auto&& val : values) {
+                val.second /= mBuffer.size();
+            }
+
+            // Find classification with max
+            float max = 0;
+            std::string label;
+            for (auto &&item : values) {
+                if(item.second > max) {
+                    max = item.second;
+                    label = item.first;
+                }
+            }
+
+            Text::access access2 = text->getAccess(ACCESS_READ_WRITE);
+            char buffer[8];
+            std::sprintf(buffer, "%.2f", max);
+            std::string result = label + ": " + buffer;
+            access2->setData(result);
+        }
 };
 
 }
