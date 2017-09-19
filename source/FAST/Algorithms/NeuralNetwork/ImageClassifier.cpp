fa34a6602cfe0b00eeac21219742eba836ac6e25
Erik Smistad <ersmistad@gmail.com>
2017-09-19 15:04:31 +0200

    Cleaned up classification to text code and added attribute

--- source/FAST/Algorithms/NeuralNetwork/ImageClassifier.cpp
+++ source/FAST/Algorithms/NeuralNetwork/ImageClassifier.cpp
@@ -45,4 +45,60 @@ void ImageClassifier::loadAttributes() {
 	setLabels(getStringListAttribute("labels"));
 }
 
+ClassificationToText::ClassificationToText() {
+    createInputPort<ImageClassification>(0);
+    createOutputPort<Text>(0);
+    createIntegerAttribute("average_size", "Average size", "nr of frames to average", 100);
+}
+
+void ClassificationToText::loadAttributes() {
+    mBufferSize = getIntegerAttribute("average_size");
+}
+
+void ClassificationToText::execute() {
+    ImageClassification::pointer classification = getInputData<ImageClassification>();
+    Text::pointer text = getOutputData<Text>();
+
+    ImageClassification::access access = classification->getAccess(ACCESS_READ);
+    std::map<std::string, float> values = access->getData();
+
+    // Add to buffer
+    mBuffer.push_back(values);
+
+    // Remove item from buffer
+    if(mBuffer.size() > mBufferSize)
+        mBuffer.pop_front();
+
+    // Calculate average
+    for(auto&& val : values)
+       val.second = 0;
+
+    // Sum
+    for(auto sample : mBuffer) {
+        for(auto val : sample) {
+            values[val.first] += val.second;
+        }
+    }
+    // Divide
+    for(auto&& val : values) {
+        val.second /= mBuffer.size();
+    }
+
+    // Find classification with max
+    float max = 0;
+    std::string label;
+    for (auto &&item : values) {
+        if(item.second > max) {
+            max = item.second;
+            label = item.first;
+        }
+    }
+
+    Text::access access2 = text->getAccess(ACCESS_READ_WRITE);
+    char buffer[8];
+    std::sprintf(buffer, "%.2f", max);
+    std::string result = label + ": " + buffer;
+    access2->setData(result);
+}
+
 }
--- source/FAST/Algorithms/NeuralNetwork/ImageClassifier.hpp
+++ source/FAST/Algorithms/NeuralNetwork/ImageClassifier.hpp
@@ -41,59 +41,10 @@ class FAST_EXPORT  ClassificationToText : public ProcessObject {
     private:
         std::deque<std::map<std::string, float>> mBuffer; // used for calculating temporal average
         int mBufferSize = 100; // How large the buffer can be
-        ClassificationToText() {
-            createInputPort<ImageClassification>(0);
-            createOutputPort<Text>(0);
-            createIntegerAttribute("average_size", "Average size", "nr of frames to average", 100);
-        }
-        void loadAttributes(std::vector<Attribute> attributes) {
-            mBufferSize = getIntegerAttribute("avarage_size");
-        }
-        void execute() {
-            ImageClassification::pointer classification = getInputData<ImageClassification>();
-            Text::pointer text = getOutputData<Text>();
 
-            ImageClassification::access access = classification->getAccess(ACCESS_READ);
-            std::map<std::string, float> values = access->getData();
-
-            // Add to buffer
-            mBuffer.push_back(values);
-
-            // Remove item from buffer
-            if(mBuffer.size() > mBufferSize)
-                mBuffer.pop_front();
-
-            // Calculate average
-            for(auto&& val : values)
-               val.second = 0;
-
-            // Sum
-            for(auto sample : mBuffer) {
-                for(auto val : sample) {
-                    values[val.first] += val.second;
-                }
-            }
-            // Divide
-            for(auto&& val : values) {
-                val.second /= mBuffer.size();
-            }
-
-            // Find classification with max
-            float max = 0;
-            std::string label;
-            for (auto &&item : values) {
-                if(item.second > max) {
-                    max = item.second;
-                    label = item.first;
-                }
-            }
-
-            Text::access access2 = text->getAccess(ACCESS_READ_WRITE);
-            char buffer[8];
-            std::sprintf(buffer, "%.2f", max);
-            std::string result = label + ": " + buffer;
-            access2->setData(result);
-        }
+        ClassificationToText();
+        void loadAttributes();
+        void execute();
 };
 
 }
