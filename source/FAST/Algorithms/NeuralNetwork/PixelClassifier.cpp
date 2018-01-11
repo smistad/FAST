f5d2985f62c4833b06d324525df6adbbc634e956
Erik Smistad <ersmistad@gmail.com>
2018-01-11 11:03:59 +0100

    can now set threshold on pixel classifier

--- source/FAST/Algorithms/NeuralNetwork/PixelClassifier.cpp
+++ source/FAST/Algorithms/NeuralNetwork/PixelClassifier.cpp
@@ -10,6 +10,7 @@ PixelClassifier::PixelClassifier() {
 
     mNrOfClasses = -1;
     mHeatmapOutput = false;
+    mThreshold = 0.5;
 
     createIntegerAttribute("classes", "Classes", "Number of possible classes for each pixel", 2);
     createBooleanAttribute("heatmap_output", "Output heatmap", "Enable heatmap output instead of segmentation", false);
@@ -68,10 +69,9 @@ void PixelClassifier::execute() {
             delete[] data;
         } else {
             uchar *data = new uchar[outputWidth * outputHeight];
-            const float threshold = 0.75;
             for (int x = 0; x < outputWidth; ++x) {
                 for (int y = 0; y < outputHeight; ++y) {
-                    data[x + y * outputWidth] = tensor_mapped(0, y, x, j) > threshold ? j : 0;
+                    data[x + y * outputWidth] = tensor_mapped(0, y, x, j) > mThreshold ? j : 0;
                 }
             }
             output->create(outputWidth, outputHeight, TYPE_UINT8, 1, data);
@@ -102,6 +102,10 @@ void PixelClassifier::loadAttributes() {
     } else {
         setSegmentationOutput();
     }
+}
+
+void PixelClassifier::setThreshold(float threshold) {
+    mThreshold = threshold;
 }
 
 }
