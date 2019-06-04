f8fac29a95ccfe91cf96bd1eff85222268f0a038
Erik Smistad <ersmistad@gmail.com>
2019-06-04 16:55:28 +0200

    Fixed OpenMP issues and OpenVINO

--- cmake/ModuleOpenVINO.cmake
+++ cmake/ModuleOpenVINO.cmake
@@ -14,4 +14,8 @@ if(FAST_MODULE_OpenVINO)
     endif()
     find_package(InferenceEngine REQUIRED)
 
+    # Avoid some OpenMP issues with the ie_cpu_extensions target
+    set_target_properties(ie_cpu_extension PROPERTIES CMAKE_C_FLAGS "")
+    set_target_properties(ie_cpu_extension PROPERTIES CMAKE_CXX_FLAGS "")
+    set_target_properties(ie_cpu_extension PROPERTIES CMAKE_EXE_LINKER_FLAGS "")
 endif()
