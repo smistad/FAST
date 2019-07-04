6b2bedffff0d5c2cdf546967a8953161b36075b4
Erik Smistad <ersmistad@gmail.com>
2019-07-04 10:13:14 +0200

    Added WSI import and visualization example

---
+++ source/FAST/Examples/DataImport/importWholeSlideImage.cpp
@@ -0,0 +1,36 @@
+/**
+ * Examples/DataImport/importWholeSlideImage.cpp
+ *
+ * If you edit this example, please also update the wiki and source code file in the repository.
+ */
+#include <FAST/Tools/CommandLineParser.hpp>
+#include "FAST/Importers/WholeSlideImageImporter.hpp"
+#include "FAST/Visualization/ImagePyramidRenderer/ImagePyramidRenderer.hpp"
+#include "FAST/Visualization/SimpleWindow.hpp"
+
+using namespace fast;
+
+int main(int argc, char** argv) {
+    Reporter::setGlobalReportMethod(Reporter::COUT);
+    CommandLineParser parser("Import image from file example");
+    parser.addPositionVariable(1, "filename", Config::getTestDataPath() + "/CMU-1.tiff");
+    parser.parse(argc, argv);
+
+    // Import image from file using the ImageFileImporter
+    auto importer = WholeSlideImageImporter::New();
+    importer->setFilename(parser.get(1));
+
+    // Render
+    auto renderer = ImagePyramidRenderer::New();
+    renderer->addInputConnection(importer->getOutputPort());
+
+    // Setup window
+    auto window = SimpleWindow::New();
+    window->addRenderer(renderer);
+    window->set2DMode();
+#ifdef FAST_CONTINUOUS_INTEGRATION
+    // This will automatically close the window after 5 seconds, used for CI testing
+    window->setTimeout(5*1000);
+#endif
+    window->start();
+}
