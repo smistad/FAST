e59f80c7513cce156ecc6fe738a4630ae86c2304
Erik Smistad <ersmistad@gmail.com>
2020-01-02 14:50:59 +0100

    minor fixes on SegmentationVolumeReconstructor

--- source/FAST/Algorithms/SegmentationVolumeReconstructor/Tests.cpp
+++ source/FAST/Algorithms/SegmentationVolumeReconstructor/Tests.cpp
@@ -17,13 +17,12 @@ TEST_CASE("Segmentation volume reconstructor", "[SegmentationVolumeReconstructor
     //streamer->setFilenameFormat("/home/smistad/data/2018-05-23_ROMO_Phantom_test.cx3/US_Acq/US-Acq_1_20180523T132954/US-Acq_1_20180523T132954_Sonix_#.mhd");
     //streamer->setTimestampFilename("/home/smistad/data/2018-05-23_ROMO_Phantom_test.cx3/US_Acq/US-Acq_1_20180523T132954/US-Acq_1_20180523T132954_Sonix.fts");
 
-    PixelClassifier::pointer segmentation = PixelClassifier::New();
-    segmentation->setNrOfClasses(3);
+    auto segmentation = SegmentationNetwork::New();
     segmentation->load(Config::getTestDataPath() + "NeuralNetworkModels/jugular_vein_segmentation.pb");
     //segmentation->setNrOfClasses(2);
     //segmentation->load("/home/smistad/workspace/AAA-Ultrasound/phantom/models/phantom.pb");
     segmentation->setScaleFactor(1.0f / 255.0f);
-    segmentation->addOutputNode(0, "conv2d_23/truediv");
+    segmentation->setOutputNode(0, "conv2d_23/truediv");
     segmentation->setInputConnection(streamer->getOutputPort());
 
     SegmentationVolumeReconstructor::pointer reconstructor = SegmentationVolumeReconstructor::New();
