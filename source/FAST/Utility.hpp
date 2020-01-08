7ad83efdf654cf0ac3d3fa4baf4c151398340c1c
Erik Smistad <ersmistad@gmail.com>
2020-01-08 12:57:28 +0100

    removed std functions deprecated in C++17

--- source/FAST/Utility.hpp
+++ source/FAST/Utility.hpp
@@ -120,13 +120,13 @@ FAST_EXPORT std::vector<std::string> split(const std::string input, const std::s
 // trim from start (in place)
 static inline void ltrim(std::string &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-                                    std::not1(std::ptr_fun<int, int>(std::isspace))));
+        [](unsigned char c) {return !std::isspace(c); }));
 }
 
 // trim from end (in place)
 static inline void rtrim(std::string &s) {
     s.erase(std::find_if(s.rbegin(), s.rend(),
-                         std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        [](unsigned char c) {return !std::isspace(c); }).base(), s.end());
 }
 
 // trim from both ends (in place)
