fcf9a5429874218e2024a68be14cfa9d3c65a7d7
Erik Smistad <ersmistad@gmail.com>
2020-02-18 13:45:44 +0100

    Updated OpenVINO API to version 2020.1

--- cmake/ExternalOpenVINO.cmake
+++ cmake/ExternalOpenVINO.cmake
@@ -8,17 +8,16 @@ ExternalProject_Add(OpenVINO
         PREFIX ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO
         BINARY_DIR ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/build
         GIT_REPOSITORY "https://github.com/opencv/dldt.git"
-        GIT_TAG "2019_R1.1"
-        SOURCE_SUBDIR inference-engine
+        GIT_TAG "2020.1"
         UPDATE_COMMAND
-            git submodule init COMMAND git submodule update --recursive COMMAND mkdir -p inference-engine/build/
+            git submodule update --init --recursive
         CMAKE_ARGS
+            -DENABLE_BEH_TESTS=OFF
+            -DENABLE_C=OFF
+            -DENABLE_FUNCTIONAL_TESTS=OFF
             -DENABLE_OPENCV:BOOL=OFF
-            -DENABLE_OBJECT_DETECTION_TESTS:BOOL=OFF
             -DENABLE_PROFILING_ITT:BOOL=OFF
             -DENABLE_SAMPLES:BOOL=OFF
-            -DENABLE_SAMPLES_CORE:BOOL=OFF
-            -DENABLE_SEGMENTATION_TESTS:BOOL=OFF
             -DENABLE_CPPCHECK:BOOL=OFF
             -DENABLE_CPPLINT:BOOL=OFF
             -DBUILD_TESTING:BOOL=OFF
@@ -30,19 +29,18 @@ ExternalProject_Add(OpenVINO
             -DCMAKE_INSTALL_MESSAGE:BOOL=LAZY
             -DCMAKE_INSTALL_PREFIX:STRING=${FAST_EXTERNAL_INSTALL_DIR}
         BUILD_COMMAND
-            make -j4 inference_engine clDNNPlugin myriadPlugin MKLDNNPlugin ie_cpu_extension
+            make -j4 inference_engine clDNNPlugin myriadPlugin MKLDNNPlugin
         INSTALL_COMMAND
-            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/bin/intel64/Release/lib/libinference_engine.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
-            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/bin/intel64/Release/lib/libclDNN64.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
-            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/bin/intel64/Release/lib/libclDNNPlugin.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
-            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/bin/intel64/Release/lib/libMKLDNNPlugin.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
-            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/bin/intel64/Release/lib/libmyriadPlugin.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
-            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/bin/intel64/Release/lib/libcpu_extension.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
+            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/bin/intel64/Release/lib/libinference_engine.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
+            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/bin/intel64/Release/lib/libclDNNPlugin.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
+            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/bin/intel64/Release/lib/libMKLDNNPlugin.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
+            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/bin/intel64/Release/lib/libmyriadPlugin.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
+            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/bin/intel64/Release/lib/libngraph.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
+            ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/bin/intel64/Release/lib/plugins.xml ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
             #${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/bin/intel64/Release/lib/libGNAPlugin.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
             #${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/bin/intel64/Release/lib/libHeteroPlugin.so ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
             ${CMAKE_COMMAND} -E copy ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/temp/tbb/lib/libtbb.so.2 ${FAST_EXTERNAL_INSTALL_DIR}/lib/ COMMAND
             ${CMAKE_COMMAND} -E copy_directory ${FAST_EXTERNAL_BUILD_DIR}/OpenVINO/src/OpenVINO/inference-engine/include/ ${FAST_EXTERNAL_INSTALL_DIR}/include/openvino/
-
         )
 endif()
 
