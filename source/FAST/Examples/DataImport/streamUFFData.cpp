aa1bfe1a692d225332881b305c651c69364c3505
Erik Smistad <ersmistad@gmail.com>
2020-03-02 20:41:59 +0100

    Added the UFFstreamer and stream UFF data example using the new HDF5 module.

---
+++ source/FAST/Examples/DataImport/streamUFFData.cpp
@@ -0,0 +1,27 @@
+#include <FAST/Testing.hpp>
+#include <FAST/Streamers/UFFStreamer.hpp>
+#include <FAST/Visualization/ImageRenderer/ImageRenderer.hpp>
+#include <FAST/Visualization/SimpleWindow.hpp>
+#include <FAST/Tools/CommandLineParser.hpp>
+
+using namespace fast;
+
+int main(int argc, char** argv) {
+	CommandLineParser parser("Stream UFF data");
+	parser.addPositionVariable(1, "filename", Config::getTestDataPath() + "US/b_data_IQ022_A4C.uff");
+	parser.parse(argc, argv);
+
+	auto streamer = UFFStreamer::New();
+	streamer->setFilename(parser.get("filename"));
+
+	auto renderer = ImageRenderer::New();
+	renderer->addInputConnection(streamer->getOutputPort());
+	renderer->setIntensityLevel(500000);
+	renderer->setIntensityWindow(1000000);
+
+	auto window = SimpleWindow::New();
+	window->addRenderer(renderer);
+	window->set2DMode();
+	window->getView()->setBackgroundColor(Color::Black());
+	window->start();
+}
