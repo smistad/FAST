9eee5af0448123023320af731e4f68b20b3ddf82
Erik Smistad <ersmistad@gmail.com>
2020-06-08 13:22:23 +0200

    fixed compile error in bounding box renderer tests

--- source/FAST/Visualization/BoundingBoxRenderer/BoundingBoxRendererTests.cpp
+++ source/FAST/Visualization/BoundingBoxRenderer/BoundingBoxRendererTests.cpp
@@ -9,15 +9,15 @@ TEST_CASE("BoundingBox renderer", "[BoundingBoxRenderer][fast][visual]") {
 	bbset->create();
 	{
 		auto access = bbset->getAccess(ACCESS_READ_WRITE);
-		access->addBoundingBox({2.0f, 2.0f}, {10.f, 3.0f});
-		access->addBoundingBox({6.0f, 1.0f}, {3.f, 5.0f});
+		access->addBoundingBox({2.0f, 2.0f}, {10.f, 3.0f}, 1, 1);
+		access->addBoundingBox({6.0f, 1.0f}, {3.f, 5.0f}, 1, 1);
 	}
 
 	auto bbset2 = BoundingBoxSet::New();
 	bbset2->create();
 	{
 		auto access = bbset2->getAccess(ACCESS_READ_WRITE);
-		access->addBoundingBox({1.0f, 1.0f}, {3.0f, 3.0f});
+		access->addBoundingBox({1.0f, 1.0f}, {3.0f, 3.0f}, 1, 1);
 	}
 
 	auto accumulate = BoundingBoxSetAccumulator::New();
@@ -25,7 +25,6 @@ TEST_CASE("BoundingBox renderer", "[BoundingBoxRenderer][fast][visual]") {
 	auto result = accumulate->updateAndGetOutputData<BoundingBoxSet>();
 	accumulate->setInputData(bbset2);
 	accumulate->update();
-	std::cout << "Final: " << result->getNrOfLines() << " v: " << result->getNrOfVertices() << std::endl;
 
 	auto renderer = BoundingBoxRenderer::New();
 	renderer->addInputData(result);
