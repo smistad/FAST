d0ebae2b162048041837aef1d682246201f32de2
Erik Smistad <ersmistad@gmail.com>
2020-08-25 20:09:43 +0200

    updated URL to Eigen repository wich has moved

--- cmake/ExternalEigen.cmake
+++ cmake/ExternalEigen.cmake
@@ -5,9 +5,7 @@ include(cmake/Externals.cmake)
 ExternalProject_Add(eigen
         PREFIX ${FAST_EXTERNAL_BUILD_DIR}/eigen
         BINARY_DIR ${FAST_EXTERNAL_BUILD_DIR}/eigen
-        #GIT_REPOSITORY "https://github.com/RLovelett/eigen.git"
-        #GIT_TAG "a46d2e7337c4656f00abe54a8115f6d76153a048"
-        URL "https://bitbucket.org/eigen/eigen/get/b4890dc.tar.gz"
+        URL "https://gitlab.com/libeigen/eigen/-/archive/master/b4890dc.tar.gz"
         INSTALL_DIR ${FAST_EXTERNAL_INSTALL_DIR}
         CMAKE_CACHE_ARGS
             -DCMAKE_BUILD_TYPE:STRING=Release
