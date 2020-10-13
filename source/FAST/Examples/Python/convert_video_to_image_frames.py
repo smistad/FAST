b587bb65d4b5ceef6cecc28a91f368a344a76295
Erik Smistad <ersmistad@gmail.com>
2020-10-13 11:18:49 +0200

    improved convert video to image frames example

--- source/FAST/Examples/Python/convert_video_to_image_frames.py
+++ source/FAST/Examples/Python/convert_video_to_image_frames.py
@@ -1,6 +1,10 @@
 """
 This example loads a video and converts to a stream of image frames and display the
 individual frames with matplotlib.
+
+Note that additional depedencies are required to stream videos in FAST:
+Linux: sudo apt install libgstreamer1.0-dev libgstreamer-plugins-bad1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev
+Windows: K-lite codec pack https://codecguide.com/download_kl.htm
 """
 import fast
 import matplotlib.pyplot as plt
@@ -18,16 +22,20 @@ streamer.update() # Start pipeline
 frame_list = []
 counter = 0
 while True:
-    frame_list.append(dataChannel.getNextImage())
-    if frame_list[-1].isLastFrame():
+    frame = dataChannel.getNextImage()
+    counter += 1
+    if frame.isLastFrame():
         break
+
+    # Only show every X frame
+    if counter % 20 == 0: frame_list.append((frame, counter))
+
     if len(frame_list) == 9:
         # Display the 9 last frames
         f, axes = plt.subplots(3,3, figsize=(10,10))
         for i in range(3):
             for j in range(3):
-                axes[i, j].set_title('Frame: ' + str(counter))
-                axes[i, j].imshow(frame_list[i + j*3], cmap='gray')
-                counter += 1
+                axes[j, i].set_title('Frame: ' + str(frame_list[i + j*3][1]))
+                axes[j, i].imshow(frame_list[i + j*3][0], cmap='gray')
         plt.show()
         frame_list.clear()
