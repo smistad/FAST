72bf3b55382449c74d273021c79c61394535c61b
Andreas Østvik <a.ostvik@gmail.com>
2020-12-21 08:31:53 +0100

    Moved data structure and scan converter object to header. Added some utility functions for ultrasound processing of UFF data.

--- source/FAST/Streamers/UFFStreamer.hpp
+++ source/FAST/Streamers/UFFStreamer.hpp
@@ -2,105 +2,88 @@
 
 #include <FAST/Streamers/RandomAccessStreamer.hpp>
 
+
 namespace fast {
 
-class UFFData;
+class UFFReader;
+
+double linearInterpolate(double a, double b, double t);
+void cart2pol(double x, double y, double &r, double &th);
+void pol2cart(double r, double th, double &x, double &y);
+unsigned int normalizeToGrayScale(double dBPixel, int dynamic_range = 60, int gain = 10);
+
+
+/**
+* A data structure for the ultrasound file format (UFF).
+* Stores relevant parameters.
+*/
+
+struct UFFData {
+    Vector3f spacing;
+    std::string groupName;
+    std::string x_axis_name, y_axis_name;
+    uint width;
+    uint height;
+    bool isScanConverted;
+    std::string dataGroupName;
+    int numFrames;
+    bool polarCoordinates;
+    std::vector<std::vector<unsigned char> > dataScanconverted;
+    std::vector<std::vector<std::complex<double> > > iqData;
+
+    std::vector<double> azimuth_axis;
+    std::vector<double> depth_axis;
+};
+
 
 /**
 * A streamer for reading data stored in the ultrasound file format (UFF)
 * which is essentially and HDF5 file with ultrasound image/beam data.
 */
 class FAST_EXPORT UFFStreamer : public RandomAccessStreamer {
-    struct FAST_EXPORT UFFData {
-        struct spacingStruct {
-            double x = 0;
-            double y = 0;
-            double z = 0;
-        };
+    FAST_OBJECT(UFFStreamer)
 
-        std::string groupName;
-        std::string x_axis_name, y_axis_name;
-        uint width;
-        uint height;
-        spacingStruct spacing;
-        bool isScanconverted;
-        std::string dataGroupName;
-        int numFrames;
-        bool polarCoordinates;
-        std::vector<std::vector<unsigned char> > dataScanconverted;
-        std::vector<std::vector<std::complex<double> > > iqData;
-
-        std::vector<double> azimuth_axis;
-        std::vector<double> depth_axis;
-    };
-
-    class UFFReader {
     public:
-        UFFReader();
+        UFFStreamer();
+        void setFilename(std::string filename);
+        void execute() override;
+        int getNrOfFrames() override;
+        // Set name of which HDF5 group to stream
+        void setName(std::string name);
+        void loadAttributes() override;
+        ~UFFStreamer();
 
-        void open(std::string filename);
+    protected:
+        void generateStream() override;
+        std::string m_filename;
+        std::string m_name;
+        UFFData m_uffData;
+};
 
-        void close();
 
-        std::string findHDF5BeamformedDataGroupName();
-
-        UFFData getUFFData();
-
-    private:
-        H5::H5File mFile;
-
-        void getAxisNames(H5::Group scanGroup, UFFData &dataStruct);
-
-        void getImageSize(H5::Group scanGroup, UFFData &dataStruct);
-
-        void getSpacing(H5::Group scanGroup, UFFData &dataStruct);
-
-        H5::Group getDataGroupAndIsScanconverted(UFFData &dataStruct);
-
-        void readData(H5::Group dataGroup, UFFData &dataStruct);
-
-        void readNotScanconvertedData(H5::Group dataGroup, UFFData &dataStruct);
-
-        void readScanconvertedData(H5::Group dataGroup, UFFData &dataStruct);
-
-    };
-
-    double linearInterpolate(double a, double b, double t);
-
-    void cart2pol(double x, double y, double &r, double &th);
-
-    void pol2cart(double r, double th, double &x, double &y);
-
-    unsigned int normalizeToGrayScale(double dBPixel, int dynamic_range = 60, int gain = 10);
-
-    class ScanConvertion {
+/**
+* A scan converter for data from the ultrasound file format (UFF).
+*
+*/
+class UFFScanConvert {
     public:
-        ScanConvertion();
-
+        UFFScanConvert();
         void loadData(UFFData &uffData);
-
         bool scanConvert(int newWidth, int newHeight, bool linearInterpolation);
-
         UFFData getUffData();
 
     protected:
-        UFFData mUFFData;
+        UFFData m_uffData;
         std::vector<std::vector<double> > mBeamData;
 
         bool scanConvertCartesianCoordinates(int newWidth = 512, int newHeight = 512, bool linearInterpolation = true);
-
         bool scanConvertPolarCoordinates(int newWidth = 512, int newHeight = 512, bool linearInterpolation = true);
-
         void normalizeEnvelopeAndLogCompress();
-
         std::complex<double> findMax();
-
         double getCartesianPixelValue(double xIq, double yIq, int frameNr, bool linear);
-
         double getPixelValue(double radius, double theta, int frameNr, bool linear = true);
+        void getIteratorToElementAfterValue(double value, std::vector<double> &vector, std::vector<double>::iterator &iter);
+};
 
-        void
-        getIteratorToElementAfterValue(double value, std::vector<double> &vector, std::vector<double>::iterator &iter);
-    };
 
 }
