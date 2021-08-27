83d7489c7e80ebc3e28ac6ca2e13e0424f9d094e
Erik Smistad <ersmistad@gmail.com>
2021-08-27 11:48:35 +0200

    Fixed windows python issue in ImageChannelConverter

--- source/FAST/Algorithms/ImageChannelConverter/ImageChannelConverter.hpp
+++ source/FAST/Algorithms/ImageChannelConverter/ImageChannelConverter.hpp
@@ -12,7 +12,7 @@ class FAST_EXPORT ImageChannelConverter : public ProcessObject {
     FAST_PROCESS_OBJECT(ImageChannelConverter)
     public:
         FAST_CONSTRUCTOR(ImageChannelConverter,
-                         std::vector<int>, channelsToRemove, = {},
+                         std::vector<int>, channelsToRemove, = std::vector<int>(),
                          bool, reverse, = false)
         void setChannelsToRemove(bool channel1, bool channel2, bool channel3, bool channel4);
         void setReverseChannels(bool reverse);
