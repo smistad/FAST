5bc7872632f5c806269e424ae30bd3c11db02066
Erik Smistad <ersmistad@gmail.com>
2021-08-27 08:03:02 +0200

    Fixed TensorFlow missing symbols issue

--- cmake/ModuleTensorFlow.cmake
+++ cmake/ModuleTensorFlow.cmake
@@ -7,13 +7,13 @@ if(FAST_MODULE_TensorFlow)
         fast_download_dependency(tensorflow
                 2.4.0
                 ff6b590025dd7b9a736987ff082ef0af20fe4f91a9b3fe846d25828d806a094b
-                tensorflow_cc.lib
         )
+        set(TensorFlow_LIBRARIES tensorflow_cc.lib)
     else()
         fast_download_dependency(tensorflow
                 2.4.0
-                5d548ad173df78cc736ed50820b6a5af77764763d727c9a601da2f4a7c5b2f06
-                libtensorflow_cc.so
+                58c3988c5f5b95d49cdb87d3b42f665691fc6975ab2272ab581a31882364fab3
         )
+        set(TensorFlow_LIBRARIES tensorflow_cc.so)
     endif()
 endif()
