e8244dc844d87123201b6084a704c4d0ac92b240
Erik Smistad <ersmistad@gmail.com>
2021-10-08 14:38:41 +0200

    Try to set environment variable osx deployment target for python wheel

--- cmake/PythonWheel.cmake
+++ cmake/PythonWheel.cmake
@@ -1,5 +1,8 @@
 message(STATUS "Creating setup.py for python ${PYTHON_VERSION} wheel...")
 
 configure_file(${FAST_SOURCE_DIR}/source/FAST/Python/setup.py.in ${FAST_BINARY_DIR}/python/setup.py @ONLY IMMEDIATE)
-
+if(APPLE)
+execute_process(COMMAND MACOSX_DEPLOYMENT_TARGET=10.13 ${PYTHON_EXECUTABLE} setup.py bdist_wheel --py-limited-api cp36 WORKING_DIRECTORY "${FAST_BINARY_DIR}/python/")
+else()
 execute_process(COMMAND ${PYTHON_EXECUTABLE} setup.py bdist_wheel --py-limited-api cp36 WORKING_DIRECTORY "${FAST_BINARY_DIR}/python/")
+endif()
