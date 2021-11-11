cb78b63948a1adc9147b5ea10c18c17ab01f984c
Erik Smistad <ersmistad@gmail.com>
2021-11-11 13:43:39 +0100

    Added possibility of specifying which channels to use in TensorToImage

--- source/FAST/Algorithms/NeuralNetwork/TensorToImage.cpp
+++ source/FAST/Algorithms/NeuralNetwork/TensorToImage.cpp
@@ -4,9 +4,12 @@
 
 namespace fast {
 
-TensorToImage::TensorToImage() {
+TensorToImage::TensorToImage(std::vector<int> channels) {
     createInputPort<Tensor>(0);
     createOutputPort<Image>(0);
+    m_channels = channels;
+
+    createIntegerAttribute("channels", "Channels", "Channels to use from tensor", -1);
 }
 
 void TensorToImage::execute() {
@@ -14,7 +17,7 @@ void TensorToImage::execute() {
     const auto shape = tensor->getShape();
     auto access = tensor->getAccess(ACCESS_READ);
     const int dims = shape.getDimensions();
-    const int channels = shape[dims-1];
+    const int channelsInTensor = shape[dims-1];
     const int outputWidth = shape[dims-2];
     const int outputHeight = shape[dims-3];
     int outputDepth = 1;
@@ -24,14 +27,50 @@ void TensorToImage::execute() {
     float* tensorData = access->getRawData();
 
     Image::pointer image;
-    if(outputDepth == 1) {
-        image = Image::create(outputWidth, outputHeight, TYPE_FLOAT, channels, std::move(tensorData));
+    if(m_channels.empty()) {
+        if(outputDepth == 1) {
+            image = Image::create(outputWidth, outputHeight, TYPE_FLOAT, channelsInTensor, tensorData);
+        } else {
+            image = Image::create(outputWidth, outputHeight, outputDepth, TYPE_FLOAT, channelsInTensor, tensorData);
+        }
     } else {
-        image = Image::create(outputWidth, outputHeight, outputDepth, TYPE_FLOAT, channels, std::move(tensorData));
+        // Select specific channels from tensor data
+        auto newTensorData = make_uninitialized_unique<float[]>(outputWidth*outputHeight*outputDepth*m_channels.size());
+        if(outputDepth == 1) {
+            auto tensorData = access->getData<3>();
+            for(int channel : m_channels) {
+                Eigen::array<int, 3> offsets = {0, 0, channel};
+                Eigen::array<int, 3> extents = {outputHeight, outputWidth, 1};
+                Eigen::Tensor<float, 3, Eigen::RowMajor> res = tensorData.slice(offsets, extents);
+                std::memcpy(&newTensorData[outputWidth*outputHeight*channel], res.data(), sizeof(float)*outputWidth*outputHeight);
+            }
+            image = Image::create(outputWidth, outputHeight, TYPE_FLOAT, m_channels.size(), std::move(newTensorData));
+        } else {
+            auto tensorData = access->getData<4>();
+            for(int channel : m_channels) {
+                Eigen::array<int, 4> offsets = {0, 0, 0, channel};
+                Eigen::array<int, 4> extents = {outputDepth, outputHeight, outputWidth, 1};
+                Eigen::Tensor<float, 4, Eigen::RowMajor> res = tensorData.slice(offsets, extents);
+                std::memcpy(&newTensorData[outputWidth*outputHeight*outputDepth*channel], res.data(), sizeof(float)*outputWidth*outputHeight*outputDepth);
+            }
+            image = Image::create(outputWidth, outputHeight, outputDepth, TYPE_FLOAT, m_channels.size(), std::move(newTensorData));
+        }
     }
     image->setSpacing(tensor->getSpacing());
     SceneGraph::setParentNode(image, tensor);
     addOutputData(0, image);
+}
+
+void TensorToImage::loadAttributes() {
+    auto channels = getIntegerListAttribute("channels");
+    if(!channels.empty() && channels[0] >= 0) {
+        setChannels(channels);
+    }
+}
+
+void TensorToImage::setChannels(std::vector<int> channels) {
+    m_channels = channels;
+    setModified(true);
 }
 
 }
--- source/FAST/Algorithms/NeuralNetwork/TensorToImage.hpp
+++ source/FAST/Algorithms/NeuralNetwork/TensorToImage.hpp
@@ -12,9 +12,18 @@ namespace fast {
 class FAST_EXPORT TensorToImage : public ProcessObject {
     FAST_PROCESS_OBJECT(TensorToImage)
     public:
-        FAST_CONSTRUCTOR(TensorToImage)
+        /**
+         * @brief Create instance
+         * @param channels which channels to extract from tensor.
+         *      If empty all channels are used. Nr of channels has to be <= 4
+         * @return instance
+         */
+        FAST_CONSTRUCTOR(TensorToImage, std::vector<int>, channels, = std::vector<int>())
+        void setChannels(std::vector<int> channels);
+        void loadAttributes();
     protected:
         void execute() override;
+        std::vector<int> m_channels;
 };
 
 }
