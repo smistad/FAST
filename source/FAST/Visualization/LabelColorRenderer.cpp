b681f5bc6e163069fbe59a61702549b1773f1cf5
André Pedersen <andrped94@gmail.com>
2021-11-18 06:13:17 +0100

    added fix for setting colors in SegmentationRenderer after POs have been added.

--- source/FAST/Visualization/LabelColorRenderer.cpp
+++ source/FAST/Visualization/LabelColorRenderer.cpp
@@ -3,41 +3,39 @@
 namespace fast {
 
 void LabelColorRenderer::createColorUniformBufferObject() {
-    if (m_colorsModified) {
-        // Create UBO for colors
-        glDeleteBuffers(1, &m_colorsUBO);
-        glGenBuffers(1, &m_colorsUBO);
-        int maxLabel = 0;
-        for (auto &&labelColor : m_labelColors) {
-            if (labelColor.first > maxLabel)
-                maxLabel = labelColor.first;
-        }
-        auto colorData = std::make_unique<float[]>((maxLabel + 1) * 4);
-        colorData[0] = 1.0f;
-        colorData[1] = 0.0f;
-        colorData[2] = 0.0f;
-        colorData[3] = 1.0f;
-        for(int i = 1; i <= maxLabel; ++i) {
-            if (m_labelColors.count(i) > 0) {
-                colorData[i * 4 + 0] = m_labelColors[i].getRedValue();
-                colorData[i * 4 + 1] = m_labelColors[i].getGreenValue();
-                colorData[i * 4 + 2] = m_labelColors[i].getBlueValue();
-            } else {
-                colorData[i * 4 + 0] = m_defaultColor.getRedValue();
-                colorData[i * 4 + 1] = m_defaultColor.getGreenValue();
-                colorData[i * 4 + 2] = m_defaultColor.getBlueValue();
-            }
-            colorData[i * 4 + 3] = 1.0f;
-        }
-        glBindBuffer(GL_UNIFORM_BUFFER, m_colorsUBO);
-        glBufferData(GL_UNIFORM_BUFFER, sizeof(float) * 4 * (maxLabel + 1), colorData.get(), GL_STATIC_DRAW);
-        glBindBuffer(GL_UNIFORM_BUFFER, 0);
-        m_colorsModified = false;
+    // Create UBO for colors
+    glDeleteBuffers(1, &m_colorsUBO);
+    glGenBuffers(1, &m_colorsUBO);
+    int maxLabel = 0;
+    for (auto &&labelColor : m_labelColors) {
+        if (labelColor.first > maxLabel)
+            maxLabel = labelColor.first;
     }
+    auto colorData = std::make_unique<float[]>((maxLabel + 1) * 4);
+    colorData[0] = 1.0f;
+    colorData[1] = 0.0f;
+    colorData[2] = 0.0f;
+    colorData[3] = 1.0f;
+    for(int i = 1; i <= maxLabel; ++i) {
+        if (m_labelColors.count(i) > 0) {
+            colorData[i * 4 + 0] = m_labelColors[i].getRedValue();
+            colorData[i * 4 + 1] = m_labelColors[i].getGreenValue();
+            colorData[i * 4 + 2] = m_labelColors[i].getBlueValue();
+        } else {
+            colorData[i * 4 + 0] = m_defaultColor.getRedValue();
+            colorData[i * 4 + 1] = m_defaultColor.getGreenValue();
+            colorData[i * 4 + 2] = m_defaultColor.getBlueValue();
+        }
+        colorData[i * 4 + 3] = 1.0f;
+    }
+    glBindBuffer(GL_UNIFORM_BUFFER, m_colorsUBO);
+    glBufferData(GL_UNIFORM_BUFFER, sizeof(float) * 4 * (maxLabel + 1), colorData.get(), GL_STATIC_DRAW);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
 }
 
 void LabelColorRenderer::setColor(int label, Color color) {
     m_labelColors[label] = color;
+    setModified(true);
 }
 
 LabelColorRenderer::~LabelColorRenderer() noexcept {
@@ -48,6 +46,7 @@ void LabelColorRenderer::setColors(std::map<uint, Color> colors) {
     m_labelColors = std::move(colors);
     if(m_labelColors.count(255) == 0)
         m_labelColors[255] = Color::Cyan();
+    setModified(true);
 }
 
 }
