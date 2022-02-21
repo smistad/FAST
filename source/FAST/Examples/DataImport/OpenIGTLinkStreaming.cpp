da1ef9c678b51178fdaf8b0a70061b7561a47e2e
Erik Smistad <ersmistad@gmail.com>
2022-02-21 11:08:17 +0100

    Added device name option to OpenIGTLink C++ example

--- source/FAST/Examples/DataImport/OpenIGTLinkStreaming.cpp
+++ source/FAST/Examples/DataImport/OpenIGTLinkStreaming.cpp
@@ -9,18 +9,19 @@ int main(int argc, char** argv) {
 
 	CommandLineParser parser("OpenIGTLink streaming example");
 	parser.addPositionVariable(1, "address", "localhost", "Address of OpenIGTLink server to connect to.");
+	parser.addVariable("device-name", "default", "Device name to stream. If \"default\", one IMAGE stream will be selected");
 
 	parser.parse(argc, argv);
 
 	Reporter::setGlobalReportMethod(Reporter::COUT);
 	auto streamer = OpenIGTLinkStreamer::create(parser.get("address"));
 
-//	auto renderer = ImageRenderer::create()
-//		->connect(streamer, streamer->getOutputPortNumber("BMode"));
-	auto renderer2 = ImageRenderer::create()
-		->connect(streamer);
+	auto deviceName = parser.get("device-name");
+	deviceName = deviceName == "default" ? "" : deviceName;
+	auto renderer = ImageRenderer::create();
+	renderer->connect(streamer, streamer->getOutputPortNumber(deviceName));
 
 	SimpleWindow2D::create()
-		->connect( renderer2 )
+		->connect(renderer)
 		->run();
 }
