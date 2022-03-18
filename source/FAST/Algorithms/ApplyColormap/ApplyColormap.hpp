e355b4531d63d399ad7d13481ff5e5f38bc14016
Erik Smistad <ersmistad@gmail.com>
2022-03-18 15:54:26 +0100

    ApplyColormap and Colormap documentation

--- source/FAST/Algorithms/ApplyColormap/ApplyColormap.hpp
+++ source/FAST/Algorithms/ApplyColormap/ApplyColormap.hpp
@@ -5,14 +5,45 @@
 
 namespace fast {
 
+/**
+ * @brief Colormap data structure
+ *
+ * The colormap can be both in grayscale and in RGB color.
+ * The colormap can be linearly interpolated between points or just use nearest neighbor interpolation.
+ */
 class FAST_EXPORT Colormap {
     public:
+        /**
+         * @brief Create empty uninitialized colormap
+         */
         Colormap();
+        /**
+         * @brief Create a grayscale colormap
+         *
+         * @param colormap
+         * @param interpolate
+         */
         explicit Colormap(const std::map<float, float>& colormap, bool interpolate = true);
+        /**
+         * @brief Creates an RGB colormap
+         * @param colormap
+         * @param interpolate
+         */
         explicit Colormap(const std::map<float, Color>& colormap, bool interpolate = true);
+        /**
+         * @brief Creates a colormap directly from a list of floats.
+         *
+         * The floats must be intensity_source1, intensity_output1, intensity_source2, intensity_output2, ... N if you
+         * have a grayscale colormap.
+         * If you have a RGB colormap it should be: intensity_source1, red1, green1, blue1, intensity_source2, red2, green2, blue2, .... N
+         *
+         * @param values
+         * @param grayscale
+         * @param interpolate
+         */
         Colormap(std::vector<float> values, bool grayscale, bool interpolate = true);
         /**
-         * Create an OpenCL buffer from the transfer function data.
+         * @brief Create an OpenCL buffer from the colormap data.
          * @param device OpenCL device to transfer data to
          * @return OpenCL buffer
          */
@@ -27,9 +58,27 @@ class FAST_EXPORT Colormap {
         void checkData() const;
 };
 
+/**
+ * @brief Applies a colormap on an image to create a new image.
+ *
+ * Current limitations are: Input and output images can only be 2D. Output image is always of TYPE_UINT8
+ *
+ * Inputs:
+ * - 0: Image
+ *
+ * Outputs:
+ * - 0: Image
+ *
+ * @sa Colormap
+ */
 class FAST_EXPORT ApplyColormap : public ProcessObject {
     FAST_PROCESS_OBJECT(ApplyColormap)
     public:
+        /**
+         * @brief Create instance
+         * @param colormap Colormap to apply
+         * @return instance
+         */
         FAST_CONSTRUCTOR(ApplyColormap, Colormap, colormap,)
         void setColormap(Colormap colormap);
         Colormap getColormap() const;
