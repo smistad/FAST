4b7e44ce68268a19d9a94239757efed16e0e9686
Erik Smistad <ersmistad@gmail.com>
2022-04-28 15:06:22 +0200

    Wrote first python test and added it to CI-ubuntu workflow.

---
+++ source/FAST/Python/Tests/test_array_interface.py
@@ -0,0 +1,69 @@
+import fast
+import numpy as np
+
+
+def test_2D_image_array_interface():
+    types_to_test = [
+        (np.uint8, fast.TYPE_UINT8, 255),
+        (np.float32, fast.TYPE_FLOAT, 1),
+        (np.uint16, fast.TYPE_UINT16, 128),
+        (np.int16, fast.TYPE_INT16, 128)
+    ]
+    width = 64
+    height = 37
+
+    for np_type, fast_type, scale in types_to_test:
+        # Test no channels specified
+        data = (np.random.rand(height, width)*scale).astype(np_type)
+        image = fast.Image.createFromArray(data)
+
+        assert image.getWidth() == width
+        assert image.getHeight() == height
+        assert image.getNrOfChannels() == 1
+        assert image.getDataType() == fast_type
+        assert np.array_equal(np.asarray(image), data.reshape((height, width, 1)))
+
+        for channels in range(1, 5):
+            data = (np.random.rand(height, width, channels)*scale).astype(np_type)
+            image = fast.Image.createFromArray(data)
+
+            assert image.getWidth() == width
+            assert image.getHeight() == height
+            assert image.getNrOfChannels() == channels
+            assert image.getDataType() == fast_type
+            assert np.array_equal(np.asarray(image), data)
+
+
+def test_3D_image_array_interface():
+    types_to_test = [
+        (np.uint8, fast.TYPE_UINT8, 255),
+        (np.float32, fast.TYPE_FLOAT, 1),
+        (np.uint16, fast.TYPE_UINT16, 128),
+        (np.int16, fast.TYPE_INT16, 128)
+    ]
+    width = 64
+    height = 37
+    depth = 89
+
+    for np_type, fast_type, scale in types_to_test:
+        # Test no channels specified
+        data = (np.random.rand(depth, height, width)*scale).astype(np_type)
+        image = fast.Image.createFromArray(data)
+
+        assert image.getWidth() == width
+        assert image.getHeight() == height
+        assert image.getDepth() == depth
+        assert image.getNrOfChannels() == 1
+        assert image.getDataType() == fast_type
+        assert np.array_equal(np.asarray(image), data.reshape((depth, height, width, 1)))
+
+        for channels in range(1, 5):
+            data = (np.random.rand(depth, height, width, channels)*scale).astype(np_type)
+            image = fast.Image.createFromArray(data)
+
+            assert image.getWidth() == width
+            assert image.getHeight() == height
+            assert image.getDepth() == depth
+            assert image.getNrOfChannels() == channels
+            assert image.getDataType() == fast_type
+            assert np.array_equal(np.asarray(image), data)
