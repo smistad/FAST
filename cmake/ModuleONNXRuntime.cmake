029abb849e77667638df62c3899d55d2ccc0abe4
Erik Smistad <ersmistad@gmail.com>
2022-10-26 14:36:54 +0200

    Updated onnxruntime dependency package

--- cmake/ModuleONNXRuntime.cmake
+++ cmake/ModuleONNXRuntime.cmake
@@ -2,6 +2,6 @@ if(FAST_MODULE_ONNXRuntime AND WIN32)
     message("-- Enabling Microsoft ONNX runtime inference engine module")
     fast_download_dependency(onnxruntime
           1.13.1
-          ca1a7890391a641a283eb0de18280094f275a9ce77c39745b31484d70107fc47
+          07ab6a1e875fbd336fd694cf71728535d935bf1a87c80cb1307e04758bce87d9
     )
 endif()
