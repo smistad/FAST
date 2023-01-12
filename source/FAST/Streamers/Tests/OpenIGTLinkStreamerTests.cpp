3b3fb328492963f692675be7ff991627d1fd78c8
androst <a.ostvik@gmail.com>
2023-01-12 15:53:09 +0100

    Started work on a simple test for checking if IGTL string message are received.

--- source/FAST/Streamers/Tests/OpenIGTLinkStreamerTests.cpp
+++ source/FAST/Streamers/Tests/OpenIGTLinkStreamerTests.cpp
@@ -4,6 +4,7 @@
 #include "FAST/Visualization/ImageRenderer/ImageRenderer.hpp"
 #include "FAST/Visualization/SimpleWindow.hpp"
 #include "FAST/Algorithms/AddTransformation/AddTransformation.hpp"
+#include <FAST/Algorithms/Lambda/RunLambda.hpp>
 
 using namespace fast;
 
@@ -29,6 +30,28 @@ TEST_CASE("Stream 2D images using OpenIGTLinkStreamer", "[OpenIGTLinkStreamer][f
     renderer->addInputConnection(addTransformation->getOutputPort());
     auto window = SimpleWindow::New();
     window->addRenderer(renderer);
+    window->setTimeout(5000);
+    CHECK_NOTHROW(window->start());
+}
+
+TEST_CASE("Stream image and string message using OpenIGTLinkStreamer", "[OpenIGTLinkStreamer][fast][IGTLink][visual]") {
+
+    auto streamer = OpenIGTLinkStreamer::create("localhost");
+
+    auto renderer = ImageRenderer::create();
+    renderer->connect(streamer, streamer->getOutputPortNumber("BMode") );
+
+    auto runLambda = RunLambda::create([](DataObject::pointer data) {
+        auto stringMessage = std::dynamic_pointer_cast<FASTString>(data);
+        if(stringMessage) {
+            std::cout << "String message: " << stringMessage->get() << std::endl;
+        }
+        std::cout << data->getNameOfClass() << std::endl;
+        return DataList(data);
+    })->connect(streamer, streamer->getOutputPortNumber("ECG"));
+
+    auto window = SimpleWindow2D::create()->connect(renderer);
+    window->addProcessObject(runLambda);
     window->setTimeout(5000);
     CHECK_NOTHROW(window->start());
 }
