eeba7c3ce450ed34f028830c04d0d087ea05e0b6
Erik Smistad <ersmistad@gmail.com>
2023-02-21 14:10:38 +0100

    Fixed bug in module plotting cmake script for arm64

--- cmake/ModulePlotting.cmake
+++ cmake/ModulePlotting.cmake
@@ -7,11 +7,19 @@ if(FAST_MODULE_Plotting)
                 JKQTCommonSharedLib_Release.lib JKQTPlotterSharedLib_Release.lib JKQTFastPlotterSharedLib_Release.lib JKQTMathTextSharedLib_Release.lib
         )
     elseif(APPLE)
+	if(CMAKE_OSX_ARCHITECTURES STREQUAL "arm64")
+        fast_download_dependency(jkqtplotter
+                2020.10
+		0d086f36ecb56411f52f711b38f131dd4b626ab266f23af63140f449f855b4cf
+                libJKQTCommonSharedLib_Release.dylib libJKQTPlotterSharedLib_Release.dylib libJKQTFastPlotterSharedLib_Release.dylib libJKQTMathTextSharedLib_Release.dylib
+	)
+	else()
         fast_download_dependency(jkqtplotter
                 2020.10
                 feb27bb5e9ac07885deab9b76aa2f266cc209e7455f8d6407ddda57b4b602e1f
                 libJKQTCommonSharedLib_Release.dylib libJKQTPlotterSharedLib_Release.dylib libJKQTFastPlotterSharedLib_Release.dylib libJKQTMathTextSharedLib_Release.dylib
                 )
+	endif()
     else()
         fast_download_dependency(jkqtplotter
                 2020.10
