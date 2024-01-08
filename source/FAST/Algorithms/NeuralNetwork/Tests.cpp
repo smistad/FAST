e878bfc41d555fdd1a9ca14255964d6416a3d3bf
Erik Smistad <ersmistad@gmail.com>
2024-01-08 13:29:20 +0100

    Disabled an onnxruntime test on linux

--- source/FAST/Algorithms/NeuralNetwork/Tests.cpp
+++ source/FAST/Algorithms/NeuralNetwork/Tests.cpp
@@ -133,6 +133,14 @@ TEST_CASE("Execute NN on single 3D image", "[fast][neuralnetwork][3d][visual]") 
 
 TEST_CASE("Multi input single output network", "[fast][neuralnetwork]") {
     for(auto& engine : InferenceEngineManager::getEngineList()) {
+#ifdef WIN32
+#elif defined(__APPLE__) || defined(__MACOSX)
+#else
+        if(engine == "ONNXRuntime") {
+            // This test hangs on linux for ONNXRuntime for some reason
+            continue;
+        }
+#endif
         auto importer = ImageFileImporter::New();
         importer->setFilename(Config::getTestDataPath() + "US/JugularVein/US-2D_0.mhd");
 
