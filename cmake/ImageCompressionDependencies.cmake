18581eb1184372969a44c405d8c85bc1b4f65203
Erik Smistad <ersmistad@gmail.com>
2024-11-20 15:19:41 +0100

    Update ImageCompressionDependencies.cmake

--- cmake/ImageCompressionDependencies.cmake
+++ cmake/ImageCompressionDependencies.cmake
@@ -1,5 +1,10 @@
 # Download dependencies required for various image compression
 if(WIN32)
+    fast_download_dependency(jpegxl
+        0.11.0
+        03e8f26bee81ca9a8186e09d9839b5176b81db5fd57324bc3007ebba9094cec0
+        jxl.lib jxl_threads.lib
+    )
 elseif(APPLE)
 else()
     fast_download_dependency(jpegxl
