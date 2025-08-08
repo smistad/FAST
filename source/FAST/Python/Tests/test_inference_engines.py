b28444c8f1070956dfc203ae58b920e068b337a7
Erik Smistad <ersmistad@gmail.com>
2025-08-08 14:30:49 +0200

    More inference engine pyfast tests

--- source/FAST/Python/Tests/test_inference_engines.py
+++ source/FAST/Python/Tests/test_inference_engines.py
@@ -1,10 +1,10 @@
 import platform
-
 import fast
 import sys
 
 
 def test_inference_engine_presence():
+    """ Test that all inference engines are loaded successfully """
     # This will load all possible inference engines:
     list = fast.InferenceEngineManager.getEngineList()
     if sys.platform == 'win32':
@@ -23,9 +23,66 @@ def test_inference_engine_presence():
         assert 'OpenVINO' in list
 
 
-def test_inference_engine_tensorflow():
+def test_inference_engine_tensorflow_download():
     """ TensorFlow is not bundled with pyfast, test that the automatic download works"""
     if not (sys.platform == 'darwin' and platform.machine() == 'arm64'):
         engine = fast.InferenceEngineManager.loadEngine('TensorFlow')
         list = fast.InferenceEngineManager.getEngineList()
         assert 'TensorFlow' in list
+
+
+def segmentation_inference(engine: str, extension: str):
+    importer = fast.ImageFileImporter.create(fast.Config.getTestDataPath() + 'US/JugularVein/US-2D_100.mhd')
+
+    segmentationNetwork = fast.SegmentationNetwork.create(
+        fast.Config.getTestDataPath() + 'NeuralNetworkModels/jugular_vein_segmentation' + extension,
+        scaleFactor=1./255.,
+        inferenceEngine=engine
+    ).connect(importer)
+
+    assert segmentationNetwork.getInferenceEngine().getName() == engine
+
+    segmentation = segmentationNetwork.runAndGetOutputData()
+    assert segmentation.getWidth() == 256
+    assert segmentation.getHeight() == 256
+    assert segmentation.calculateSumIntensity() > 0
+
+
+def test_onnxruntime_inference():
+    """ Test if ONNX Runtime is able to do inference """
+    segmentation_inference('ONNXRuntime', '.onnx')
+
+
+def test_openvino_onnx_inference():
+    """ Test if OpenVINO is able to do inference on ONNX model """
+    if sys.platform == 'darwin' and platform.machine() == 'arm64':
+        # OpenVINO not available on macOS arm64
+        return
+
+    segmentation_inference('OpenVINO', '.onnx')
+
+
+def test_openvino_ir_inference():
+    """ Test if OpenVINO is able to do inference on IR model """
+    if sys.platform == 'darwin' and platform.machine() == 'arm64':
+        # OpenVINO not available on macOS arm64
+        return
+
+    segmentation_inference('OpenVINO', '.xml')
+
+
+def test_tensorflow_inference():
+    """ Test if TensorFlow is able to do inference on protobuf model """
+    if sys.platform == 'darwin' and platform.machine() == 'arm64':
+        # TensorFlow not available on macOS arm64
+        return
+
+    segmentation_inference('TensorFlow', '.pb')
+
+
+def test_tensorrt_inference():
+    """ Test if TensorRT is able to do inference """
+    if 'TensorRT' not in fast.InferenceEngineManager.getEngineList():
+        return
+
+    segmentation_inference('TensorRT', '.onnx')
